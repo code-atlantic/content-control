51eb0b33678b2338600a2aa6eeaa5b31845cd3a6
Daniel Iser <daniel@code-atlantic.com>
2022-02-18 05:32:30 -0500

    Added base configs for tooling

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+const eslintConfig = {
+	root: true,
+	extends: [ 'plugin:@code-atlantic/eslint-plugin/recommended' ],
+	globals: {
+		wp: 'readonly',
+	},
+	env: {
+		browser: true,
+		jquery: true,
+	},
+	settings: {
+		jsdoc: {
+			mode: 'typescript',
+		},
+	},
+	rules: {},
+};
+
+module.exports = eslintConfig;
