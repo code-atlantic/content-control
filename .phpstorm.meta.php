8eb0c64d6a904cff9a6c586dfeb4c38ba0a0dbf9
danieliser <daniel@code-atlantic.com>
2022-02-25 03:39:13 -0500

    Add PHP storm meta for IDE intel

---
+++ .phpstorm.meta.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file declares all of the plugin containers available services and accessors for IDEs to read.
+ *
+ * NOTE: VS Code can use this file as well when the PHP Intelliphense extension is installed to provide autocompletion.
+ *
+ * @package ContentControl\Core
+ */
+
+namespace PHPSTORM_META;
+
+/**
+ * Provide autocompletion for plugin container access.
+ *
+ * Return lists below all must match, it cannot be defined as a variable.
+ * Thus all the duplication is needed.
+ */
+
+/**
+  * NOTE: applies specifically to using the Plugin getter directly.
+  * Example Usage: $events = pum_Scheduling_plugin()->get( 'events' );
+  */
+override( \ContentControl\Core\Plugin::get(), map( [
+  // Controllers.
+  'options'    => \ContentControl\Core\Options::class,
+] ) );
+
+ /**
+  * NOTE: applies specifically to using the global getter function.
+  * Example Usage: $events = pum_scheduling( 'events' );
+  */
+override ( \ContentControl\plugin(0), map( [
+  // Controllers.
+  'options'    => \ContentControl\Core\Options::class,
+] ) );
