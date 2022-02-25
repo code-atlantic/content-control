3bb9d4a19cad713f3cb556e952bd750f74c5d91b
danieliser <daniel@code-atlantic.com>
2022-02-25 04:46:56 -0500

    Add Controller interface

---
+++ classes/Interfaces/Controller.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin container.
+ *
+ * @copyright (c) 2021, Code Atlantic LLC.
+ *
+ * @package ContentControl
+ */
+
+namespace ContentControl\Interfaces;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Localized controller class.
+ */
+interface Controller {
+
+	/**
+	 * Handle hooks & filters or various other init tasks.
+	 *
+	 * @return void
+	 */
+	public function init();
+}
