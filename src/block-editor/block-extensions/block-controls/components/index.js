b529ba1e52c276a3246f87748f090148994ae63f
danieliser <daniel@code-atlantic.com>
2022-04-28 12:38:19 -0400

    Remove non reusable components

--- src/block-editor/block-extensions/block-controls/components/index.js
+++ src/block-editor/block-extensions/block-controls/components/index.js
@@ -1,6 +1,3 @@
 export { default as RuleGroups } from './rule-groups';
 export { default as RuleGroup } from './rule-group';
-export { default as SingleColumnItem } from './single-column-item';
-export { default as ConditionalRules } from './conditional-rules';
-export { default as DeviceRules } from './device-rules';
 export { default as DeviceToggle } from './device-toggle';
