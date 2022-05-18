19341b0c6a3291319764d311e6a4b6beed2377dd
danieliser <daniel@code-atlantic.com>
2022-05-18 01:02:45 -0400

    Refactor to use uuid and generators

--- src/block-editor/block-extensions/block-controls/components/query-builder/templates.tsx
+++ src/block-editor/block-extensions/block-controls/components/query-builder/templates.tsx
@@ -1,25 +1,30 @@
+import { __ } from '@wordpress/i18n';
 import { nanoid } from 'nanoid';
 
 /** Type Imports */
-import { Query, QueryRule, QueryGroup, QueryRuleType } from './types';
+import { QuerySet, QueryRule, QueryGroup } from './types';
 
 const newUUID = () => nanoid( 8 );
 
-export const newRule: QueryRule = {
+export const newRule = (): QueryRule => ( {
 	key: newUUID(),
 	type: 'rule',
 	name: '',
 	options: {},
 	notOperand: false,
 	logicalOperator: 'and',
-};
+} );
 
-export const newGroup: QueryGroup = {
+export const newGroup = (): QueryGroup => ( {
 	key: newUUID(),
 	type: 'group',
-	query: [ { ...newRule } ],
+	query: [ { ...newRule() } ],
 	notOperand: false,
 	logicalOperator: 'and',
-};
+} );
 
-export const newSet: Query = [ newGroup ];
+export const newSet = (): QuerySet => ( {
+	key: newUUID(),
+	label: '',
+	query: [ newRule() ],
+} );
