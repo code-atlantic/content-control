425cb8e2affbc1218bbd91ca9ec963c64148af29
danieliser <daniel@code-atlantic.com>
2022-06-10 05:11:19 -0400

    Add className prop to most components

--- src/block-editor/block-extensions/block-controls/components/query-builder/types/props.d.ts
+++ src/block-editor/block-extensions/block-controls/components/query-builder/types/props.d.ts
@@ -1,9 +1,12 @@
+import { Argument as ClassName } from 'classnames';
+
 import { Query, QueryRuleItem, QueryGroupItem, QueryItemBase } from './query';
 
 export type BuilderQueryProps< T extends Query > = {
 	query: T;
 	onChange: ( query: T ) => void;
 	indexs?: number[];
+	className: ClassName | ClassName[];
 };
 
 export type BuilderItemProps< T extends QueryItemBase > = {
