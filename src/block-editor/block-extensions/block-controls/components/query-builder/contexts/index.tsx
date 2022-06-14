f4aeeb852561173da9d7863b1d16e0d6d1080440
danieliser <daniel@code-atlantic.com>
2022-06-14 15:10:42 -0400

    Export useOptions properly

--- src/block-editor/block-extensions/block-controls/components/query-builder/contexts/index.tsx
+++ src/block-editor/block-extensions/block-controls/components/query-builder/contexts/index.tsx
@@ -1,4 +1,5 @@
 export * from './options';
 export * from './query';
 export { default as useQueryContext } from './query';
+export { default as useOptions } from './options';
 export { default as useRules } from './rules';
