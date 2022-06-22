e387c3e5d6f8f92b2964af2c13a85e11f4569f91
danieliser <daniel@code-atlantic.com>
2022-06-22 00:17:24 -0400

    Update component exports

---
+++ src/block-editor/block-extensions/block-controls/components/rule-engine/components/index.tsx
@@ -0,0 +1,7 @@
+export * from './query-item-list';
+export * from './item';
+export { default as Item } from './item';
+export { default as GroupItem } from './group-item';
+export { default as LogicalOperator } from './logical-operator';
+export { default as RuleItem } from './rule-item';
+export { default as RuleSearchBox } from './search-box';
