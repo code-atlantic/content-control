7eb568c1f6498e95419a9c1e375ba2c7b9cb2081
danieliser <daniel@code-atlantic.com>
2022-06-22 05:18:02 -0400

    Remove usage of Flex & experimental comps

--- src/block-editor/block-extensions/block-controls/components/rule-engine/components/group-item/label-control.tsx
+++ src/block-editor/block-extensions/block-controls/components/rule-engine/components/group-item/label-control.tsx
@@ -2,12 +2,7 @@
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { cancelCircleFilled, check, edit } from '@wordpress/icons';
-import {
-	Button,
-	Flex,
-	FlexItem,
-	__experimentalInputControl as InputControl,
-} from '@wordpress/components';
+import { Button, TextControl } from '@wordpress/components';
 
 const LabelControl = ( {
 	value,
@@ -18,76 +13,43 @@ const LabelControl = ( {
 	return (
 		<div className="cc-rule-engine-group-label">
 			{ null === inputText ? (
-				<Flex
-					className="cc-rule-engine-group-label__text"
-					justify="left"
-					align="center"
-				>
-					<FlexItem>
-						<h3>
-							{ value || __( 'Rule Group', 'content-control' ) }
-						</h3>
-					</FlexItem>
-					<FlexItem>
-						<Button
-							variant="link"
-							label={ __( 'Edit label', 'content-control' ) }
-							icon={ edit }
-							iconSize={ 16 }
-							onClick={ () => setInputText( value ?? '' ) }
-							isSmall={ true }
-							style={ { color: '#1d2327' } }
-						/>
-					</FlexItem>
-				</Flex>
+				<div className="cc-rule-engine-group-label__text">
+					<h3>{ value || __( 'Rule Group', 'content-control' ) }</h3>
+					<Button
+						label={ __( 'Edit label', 'content-control' ) }
+						icon={ edit }
+						iconSize={ 16 }
+						onClick={ () => setInputText( value ?? '' ) }
+					/>
+				</div>
 			) : (
 				<div className="cc-rule-engine-group-label__editor">
 					{ /* TODO Refactor this to not use Flex components */ }
-					<InputControl
+					<TextControl
 						value={ inputText }
 						onChange={ setInputText }
 						placeholder={ __( 'Group label', 'content-control' ) }
-						suffix={
-							<Flex
-								className="cc-rule-engine-label-editor__buttons"
-								gap={ 0 }
-							>
-								<FlexItem>
-									<Button
-										disabled={ value === inputText }
-										label={ __(
-											'Save',
-											'content-control'
-										) }
-										icon={ check }
-										iconSize={ 20 }
-										onClick={ () => {
-											onChange( inputText );
-											setInputText( null );
-										} }
-										isSmall={ true }
-										style={ { color: 'green' } }
-									/>
-								</FlexItem>
-
-								<FlexItem>
-									<Button
-										label={ __(
-											'Cancel',
-											'content-control'
-										) }
-										icon={ cancelCircleFilled }
-										iconSize={ 20 }
-										onClick={ () => setInputText( null ) }
-										isSmall={ true }
-										style={ {
-											color: 'lightcoral',
-										} }
-									/>
-								</FlexItem>
-							</Flex>
-						}
 					/>
+					<div className="buttons">
+						<Button
+							label={ __( 'Save', 'content-control' ) }
+							className="save-button"
+							icon={ check }
+							iconSize={ 20 }
+							onClick={ () => {
+								onChange( inputText );
+								setInputText( null );
+							} }
+							disabled={ value === inputText }
+						/>
+						<Button
+							label={ __( 'Cancel', 'content-control' ) }
+							className="cancel-button"
+							icon={ cancelCircleFilled }
+							iconSize={ 20 }
+							onClick={ () => setInputText( null ) }
+						/>
+					</div>
 				</div>
 			) }
 		</div>
