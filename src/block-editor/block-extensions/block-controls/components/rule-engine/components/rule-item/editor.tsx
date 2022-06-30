2533bc8ad85003ce828b2d509bb49498b9d4b517
danieliser <daniel@code-atlantic.com>
2022-06-30 04:56:17 -0400

    Remove button that moved to wrapper

--- src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/editor.tsx
+++ src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/editor.tsx
@@ -1,23 +1,12 @@
 /** External Imports */
 import classNames from 'classnames';
 
-/** WordPress Imports */
-import { Button } from '@wordpress/components';
-import { __ } from '@wordpress/i18n';
-import { plus } from '@wordpress/icons';
-
-/** Internal Imports */
-import { useQuery } from '../../contexts';
-import { newRule } from '../../templates';
-
 const Editor = ( {
 	ruleDef,
 	value: ruleProps,
 	onChange,
 }: ItemProps< RuleItem > ) => {
 	const { notOperand = false, name, options = {}, id } = ruleProps;
-
-	const { addItem }: QueryContextProps = useQuery();
 
 	const {
 		label = '',
@@ -80,13 +69,6 @@ const Editor = ( {
 					</div>
 				)
 			) }
-
-			<Button
-				icon={ plus }
-				iconSize={ 18 }
-				onClick={ () => addItem( newRule(), id ) }
-				label={ __( 'Add Rule', 'content-control' ) }
-			/>
 		</>
 	);
 };
