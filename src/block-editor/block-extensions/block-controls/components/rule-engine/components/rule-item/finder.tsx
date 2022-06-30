850fc8de9f320872ece4596892808f6c92703360
danieliser <daniel@code-atlantic.com>
2022-06-30 05:18:23 -0400

    Replace field with react-tags-autocomplete component

--- src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/finder.tsx
+++ src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/finder.tsx
@@ -1,10 +1,10 @@
 /** External Imports */
 import { noop } from 'lodash';
+import ReactTags from 'react-tag-autocomplete';
 
 /** WordPress Imports */
 import { __ } from '@wordpress/i18n';
 import { useState, useRef, useEffect, useMemo } from '@wordpress/element';
-import { FormTokenField } from '@wordpress/components';
 
 /** Internal Imports */
 import { useRules } from '../../contexts';
@@ -12,39 +12,39 @@ import { useRules } from '../../contexts';
 /** Styles */
 import './finder.scss';
 
-type Props = { onSelect: ( ruleName: string ) => void };
+type Props = { onSelect: ( ruleItem: Partial< RuleItem > ) => void };
 type State = {
-	text: string;
 	chosen: {
 		category: string;
 		verb: string;
 		label: string;
 	};
-	tokens: FormTokenField.Value[];
+	tags: ReactTags;
 };
 
 const Finder = ( { onSelect = noop }: Props ) => {
+	const inputRef = useRef( null );
 	const { getRuleCategories, getRulesByCategory, findRules } = useRules();
-	const containerRef = useRef( null );
 	const [ currentSearch, setCurrentSearch ] = useState< State >( {
-		text: '',
+		// Keeps record of chosen tags in RuleItem object format.
 		chosen: {
 			category: null,
 			verb: null,
 			label: null,
 		},
-		tokens: [],
+		// Current list of selected tags for react-tags fields.
+		tags: [],
 	} );
 
 	/**
-	 * Change options based on number of tokens.
+	 * Change options based on number of tags.
 	 *
 	 * 0: Nothing chosen, provide categories.
 	 * 1: Category chosen, show verbs.
 	 * 2: Only thing left to choose is a name from category/verb list.
 	 */
 	const searchOptions = useMemo( () => {
-		switch ( currentSearch.tokens.length ) {
+		switch ( currentSearch.tags.length ) {
 			default:
 			case 0:
 				return getRuleCategories();
@@ -54,9 +54,9 @@ const Finder = ( { onSelect = noop }: Props ) => {
 					currentSearch.chosen.category
 				).reduce< string[] >( ( _verbs, rule ) => {
 					if ( typeof rule.verbs !== 'undefined' ) {
-						rule.verbs.forEach( ( verb ) => {
-							if ( _verbs.indexOf( verb ) === -1 ) {
-								_verbs.push( verb );
+						rule.verbs.forEach( ( _verb ) => {
+							if ( _verbs.indexOf( _verb ) === -1 ) {
+								_verbs.push( _verb );
 							}
 						} );
 					}
@@ -69,80 +69,76 @@ const Finder = ( { onSelect = noop }: Props ) => {
 					( rule ) => rule.label
 				);
 		}
-	}, [ currentSearch.tokens, currentSearch.chosen ] );
+	}, [ currentSearch.tags ] );
 
+	/**
+	 * Focus the input when this component is rendered.
+	 */
 	useEffect( () => {
-		const firstEl = containerRef.current.querySelector(
-			'.components-form-token-field__input'
-		) as HTMLElement;
+		const firstEl = inputRef.current;
 
 		if ( null !== firstEl ) {
-			firstEl.focus();
+			firstEl.focusInput();
 		}
 	}, [] );
 
-	const [ currentToken, setCurrentToken ] = useState< string >( null );
+	const onDelete = ( tagIndex ) => {
+		setCurrentSearch( {
+			...currentSearch,
+			tags: currentSearch.tags.filter( ( _, i ) => i !== tagIndex ),
+		} );
+	};
+
+	const onAddition = ( { name: newTag } ) => {
+		const tags = [ ...currentSearch.tags, newTag ];
+		const [ category, verb, label ] = tags;
+		const chosen = {
+			category,
+			verb,
+			label,
+		};
+
+		if ( tags.length === 3 ) {
+			const chosenRule = findRules( chosen );
+
+			if ( chosenRule.length ) {
+				const { name, verbs } = chosenRule[ 0 ];
+				// Generate new rule item object.
+				onSelect( {
+					name,
+					notOperand: verbs.indexOf( verb ) === 1,
+				} );
+			}
+
+			return;
+		}
+
+		setCurrentSearch( {
+			...currentSearch,
+			chosen,
+			tags,
+		} );
+	};
 
 	return (
-		<div className="cc-rule-engine-search-box" ref={ containerRef }>
-			<FormTokenField
-				label={ null }
-				placeholder={ __( 'Type to choose a rule', 'content-control' ) }
-				isBorderless={ true }
-				tokenizeOnSpace={ ( () => {
-					const results = currentToken
-						? searchOptions.filter(
-								( option ) =>
-									option
-										.toLowerCase()
-										.indexOf(
-											currentToken.toLowerCase()
-										) >= 0
-						  )
-						: null;
-
-					console.log( results );
-
-					if ( results && results.length === 1 ) {
-						// return true;
-					}
-
-					return false;
-				} )() }
-				expandOnFocus={ true }
-				__experimentalExpandOnFocus={ true }
-				__experimentalShowHowTo={ false }
-				value={ currentSearch.tokens }
-				suggestions={ searchOptions }
-				onInputChange={ setCurrentToken }
-				onChange={ ( tokens ) => {
-					const _tokens = [
-						...tokens.map( ( token ) =>
-							typeof token === 'string' ? token : token.value
-						),
-					];
-					const [ category, verb, label ] = _tokens;
-
-					const chosen = {
-						category,
-						verb,
-						label,
-					};
-
-					setCurrentSearch( {
-						...currentSearch,
-						chosen,
-						tokens: _tokens,
-					} );
-
-					if ( _tokens.length === 3 ) {
-						const chosenRule = findRules( chosen );
-
-						if ( chosenRule.length ) {
-							onSelect( chosenRule[ 0 ].name );
-						}
-					}
-				} }
+		<div className="cc-rule-engine-search-box">
+			<ReactTags
+				placeholderText={ __( 'Select a rule', 'content-control' ) }
+				ref={ inputRef }
+				tags={ currentSearch.tags.map( ( tag, i ) => ( {
+					id: i,
+					name: tag,
+				} ) ) }
+				suggestions={ searchOptions.map( ( option, i ) => ( {
+					id: i,
+					name: option,
+				} ) ) }
+				onInput={ ( value ) => {} }
+				onAddition={ onAddition }
+				onDelete={ onDelete }
+				allowNew={ false }
+				allowBackspace={ true }
+				minQueryLength={ 0 }
 			/>
 		</div>
 	);
