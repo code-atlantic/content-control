13acd7b8cde84c4671637f962a150b6ea2d61f29
Daniel Iser <daniel@code-atlantic.com>
2022-07-21 20:55:34 +0000

    Finished filling in most of the needed methods

--- classes/Rules.php
+++ classes/Rules.php
@@ -1,21 +1,120 @@
 <?php
+/**
+ * Rule registery
+ *
+ * @package ContentControl
+ */
 
 namespace ContentControl;
 
+/**
+ * Rules registry
+ */
 class Rules {
 
-	public $data;
+	/**
+	 * Array of rules.
+	 *
+	 * @var array
+	 */
+	public $data = [];
 
+	/**
+	 * Rules constructor.
+	 */
 	public function __construct() {
 		$this->init();
 	}
 
+	/**
+	 * Set up rules list.
+	 *
+	 * @return void
+	 */
 	public function init() {
 		$rules = $this->get_built_in_rules();
 
-		$old_rules = apply_filters( 'jp_cc_registered_conditions', [] );
+		$old_rules = \ContentControl\Conditions::instance()->get_conditions();
+
+		if ( ! empty( $old_rules ) ) {
+			$old_rules = $this->parse_old_rules( $old_rules );
+
+			foreach ( $old_rules as $key => $rule ) {
+				if ( ! isset( $rules[ $key ] ) ) {
+					$rules[ $key ] = $rule;
+				}
+			}
+		}
+
+		foreach ( $rules as $rule ) {
+			$this->register_rule( $rule );
+		}
 	}
 
+	/**
+	 * Register new rule type.
+	 *
+	 * @param array $rule New rule to register.
+	 * @return void
+	 */
+	public function register_rule( $rule ) {
+		if ( $this->is_rule_valid( $rule ) ) {
+			$rule = wp_parse_args( $rule, $this->get_rule_defaults() );
+
+			$index = $rule['name'];
+			/**
+			 * In the case multiple conditions are registered with the same
+			 * identifier, we append an integer. This do/while quickly increases
+			 * the integer by one until a valid new key is found.
+			 */
+			if ( array_key_exists( $index, $this->data ) ) {
+				$i = 0;
+				do {
+					$i++;
+					$index = $rule['name'] . '-' . $i;
+				} while ( array_key_exists( $index, $this->data ) );
+			}
+
+			$indexs[]             = $index;
+			$this->data[ $index ] = $rule;
+		}
+	}
+
+	/**
+	 * Check if rule is valid.
+	 *
+	 * @param array $rule Rule to test.
+	 * @return boolean
+	 */
+	public function is_rule_valid( $rule ) {
+		return true;
+	}
+
+	/**
+	 * Get array of all registered rules.
+	 *
+	 * @return array
+	 */
+	public function get_rules() {
+		return $this->data;
+	}
+
+	/**
+	 * Get array of registered rules filtered for the block-editor.
+	 *
+	 * @return array
+	 */
+	public function get_block_editor_rules() {
+		$rules = $this->get_rules();
+
+		return apply_filters( 'contctrl_block_editor_rules', $rules );
+	}
+
+	/**
+	 * Get list of verbs.
+	 *
+	 * @return array List of verbs with translatable text.
+	 */
 	public function get_verbs() {
 		return [
 			'are'         => __( 'Are', 'content-control' ),
@@ -39,7 +138,7 @@ class Rules {
 	 *
 	 * @return array
 	 */
-	public function get_built_in_rules() {
+	private function get_built_in_rules() {
 		$verbs = $this->get_verbs();
 		return [
 			'user_has_role' => [
@@ -62,19 +161,12 @@ class Rules {
 		];
 	}
 
-	public function old_rule_defaults() {
-		return [
-			'id'       => '',
-			'callback' => null,
-			'group'    => '',
-			'name'     => '',
-			'priority' => 10,
-			'fields'   => [],
-			'advanced' => false,
-		];
-	}
-
-	public function rule_defaults() {
+	/**
+	 * Get an array of rule default values.
+	 *
+	 * @return array Array of rule default values.
+	 */
+	public function get_rule_defaults() {
 		$verbs = $this->get_verbs();
 		return [
 			'name'     => '',
@@ -89,12 +181,31 @@ class Rules {
 	}
 
 	/**
+	 * Get an array of old rule default values.
+	 *
+	 * @return array Array of old rule default values.
+	 */
+	private function get_old_rule_defaults() {
+		return [
+			'id'       => '',
+			'callback' => null,
+			'group'    => '',
+			'name'     => '',
+			'priority' => 10,
+			'fields'   => [],
+			'advanced' => false,
+		];
+	}
+
+	/**
 	 * Remaps keys & values from an old `condition` into a new `rule`.
 	 *
 	 * @param array $old_rule Old rule definition.
 	 * @return array New rule definition.
 	 */
-	public function remap_old_rule( $old_rule ) {
+	private function remap_old_rule( $old_rule ) {
+		$old_rule = wp_parse_args( $old_rule, $this->get_old_rule_defaults() );
+
 		$new_rule = [
 			'format' => '{category} {label}',
 		];
@@ -106,41 +217,34 @@ class Rules {
 			'fields'   => 'fields',
 			'callback' => 'callback',
 			'advanced' => 'frontend',
+			'priority' => 'priority',
 		];
 
 		foreach ( $remaped_keys as $old_key => $new_key ) {
 			if ( isset( $old_rule[ $old_key ] ) ) {
 				$new_rule[ $new_key ] = $old_rule[ $old_key ];
+				unset( $old_rule[ $old_key ] );
 			}
 		}
 
-		return $new_rule;
+		// Merge any leftover 'unknonw' keys, with new stuff second.
+		return array_merge( $old_rule, $new_rule );
 	}
 
-	public function registerRule( $rule ) {
-		if ( $this->isValidRule( $rule ) ) {
-			$rule = wp_parse_args( $rule, $this->rule_defaults() );
-
-			$this->data[ $rule['name'] ] = $rule;
-		}
-	}
-
-	public function isValidRule( $rule ) {
-		return true;
-	}
-
-	public function getRules() {
-		return $this->data;
-	}
-
-	public function parseOldRules( $old_rules ) {
+	/**
+	 * Parse rules that are still registered using the older deprecated methods.
+	 *
+	 * @param array $old_rules Array of old rules to manipulate.
+	 * @return array
+	 */
+	private function parse_old_rules( $old_rules ) {
 		$new_rules = [];
 
-		return $new_rules;
-	}
+		foreach ( $old_rules as $key => $old_rule ) {
+			$new_rules[ $key ] = $this->remap_old_rule( $old_rule );
+		}
 
-	public function get_block_editor_rules() {
-		return [];
+		return $new_rules;
 	}
 
 }
