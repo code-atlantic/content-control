8789cfe85b268b70d121b99011c9a5d6d0d00f22
Daniel Iser <daniel@code-atlantic.com>
2022-07-22 08:01:14 +0000

    Add new util for making rule text

--- src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/utils.tsx
+++ src/block-editor/block-extensions/block-controls/components/rule-engine/components/rule-item/utils.tsx
@@ -1,3 +1,5 @@
+import { sprintf, __ } from '@wordpress/i18n';
+
 export const formatToSprintf = ( format: string ) =>
 	format
 		.split( ' ' )
@@ -14,3 +16,30 @@ export const formatToSprintf = ( format: string ) =>
 			}
 		} )
 		.join( ' ' );
+
+export const makeRuleText = (
+	ruleDef: EngineRuleType,
+	notOperand: boolean = false
+) => {
+	const {
+		label = '',
+		category = '',
+		format = '',
+		verbs = [ '', '' ],
+	} = ruleDef ?? {};
+
+	const hasVerbs = Array.isArray( verbs ) && verbs.length >= 2;
+	let string = formatToSprintf( format );
+
+	// Only here for deprecated conditions, likely to change before final release.
+	if ( ! hasVerbs && true === notOperand ) {
+		string = __( 'Not (!): ', 'content-control' ) + string;
+	}
+
+	return sprintf(
+		string,
+		category,
+		hasVerbs ? verbs[ ! notOperand ? 0 : 1 ] : null,
+		label
+	);
+};
