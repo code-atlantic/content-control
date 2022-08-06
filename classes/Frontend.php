7d475995f5608cd8e382f724806ce6b490a599ae
Daniel Iser <daniel@code-atlantic.com>
2022-08-05 20:43:40 -0400

    Update Frontend with block method

--- classes/Frontend.php
+++ classes/Frontend.php
@@ -18,10 +18,25 @@ class Frontend {
 	/**
 	 * Initialize Hooks & Filters
 	 */
-	public function __construct() {
+		add_filter( 'pre_render_block', [ $this, 'pre_render_block' ], 10, 3 );
+
 		new Frontend\Posts();
 		new Frontend\Feeds();
 		new Frontend\Widgets();
 		new Frontend\Restrictions();
 	}
+
+	/**
+	 * Short curcuit block rendering for hidden blocks.
+	 *
+	 * @param string|null   $pre_render   The pre-rendered content. Default null.
+	 * @param array         $parsed_block The block being rendered.
+	 * @param WP_Block|null $parent_block If this is a nested block, a reference to the parent block.
+	 *
+	 * @return string|null
+	 */
+	public function pre_render_block( $pre_render, $parsed_block, $parent_block ) {
+		return $pre_render;
+	}
+
 }
