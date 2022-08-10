e33df612771e03cd5d3ba7debf519c3ff1aa13da
Daniel Iser <daniel@code-atlantic.com>
2022-08-10 09:10:55 +0000

    Add front end block condition handing

--- classes/Frontend.php
+++ classes/Frontend.php
@@ -9,24 +9,29 @@
 namespace ContentControl;
 
 use ContentControl\Interfaces\Controller;
+use ContentControl\RuleEngine\Handler;
 
 defined( 'ABSPATH' ) || exit;
 
 /**
  * Class Frontend
  */
-class Frontend extends Controller {
+class Frontend implements Controller {
 
 	/**
 	 * Initialize Hooks & Filters
 	 */
 	public function init() {
-		add_filter( 'pre_render_block', [ $this, 'pre_render_block' ], 10, 3 );
-
 		new Frontend\Posts();
 		new Frontend\Feeds();
 		new Frontend\Widgets();
 		new Frontend\Restrictions();
+
+		if ( is_admin() ) {
+			return;
+		}
+
+		add_filter( 'pre_render_block', [ $this, 'pre_render_block' ], 10, 3 );
 	}
 
 	/**
@@ -39,7 +44,33 @@ class Frontend extends Controller {
 	 * @return string|null
 	 */
 	public function pre_render_block( $pre_render, $parsed_block, $parent_block ) {
+		if ( ! isset( $parsed_block['attrs']['contentControls'] ) ) {
+			return $pre_render;
+		}
+
+		$controls = wp_parse_args( $parsed_block['attrs']['contentControls'], [
+			'enabled' => false,
+			'rules'   => [],
+		] );
+
+		if ( ! $controls['enabled'] ) {
+			return $pre_render;
+		}
+
+		$rules = wp_parse_args( $controls['rules'], [
+			'conditional' => null,
+		] );
+
+		if ( $rules['conditional'] ) {
+			$handler = new Handler( $rules['conditional']['conditionSets'], $rules['conditional']['anyAll'] );
+
+			$check = $handler->check_rules();
+
+			if ( ! $handler->check_rules() ) {
+				return '';
+			}
+		}
+
 		return $pre_render;
 	}
-
 }
