415ffae52ab347b7de9098aa47c72e0fd27715b8
Daniel Iser <daniel@code-atlantic.com>
2022-08-15 09:37:19 +0000

    Added tests for block class regex replacements

--- tests/FrontendTest.php
+++ tests/FrontendTest.php
@@ -66,4 +66,43 @@ class FrontendTest extends WP_UnitTestCase {
 		$this->assertSame( $controls, $test_block['attrs']['contentControls'] );
 	}
 
+	public function testAddBlockClasses() {
+		$test_blocks_html = [
+			'<p>Some text</p>',
+			'<div class="gallery"><img src="#" /></div>',
+			"<div class='gallery'><img src='#' /></div>",
+			'<div class="gallery with-other-class" title="Some Title"></div>',
+		];
+
+		$expected_blocks_html = [
+			'<p class="cc-hide-on-mobile">Some text</p>',
+			'<div class="gallery cc-hide-on-mobile"><img src="#" /></div>',
+			"<div class='gallery cc-hide-on-mobile'><img src='#' /></div>",
+			'<div class="gallery with-other-class cc-hide-on-mobile" title="Some Title"></div>',
+		];
+
+		$test_block = [
+			'attrs' => [
+				'contentControls' => [
+					'enabled' => true,
+					'rules'   => [
+						'device'       => [
+							'hideOn' => [
+								'mobile'  => true,
+								'tablet'  => false,
+								'desktop' => false,
+							],
+						],
+						'conditionals' => null,
+					],
+				],
+			],
+		];
+
+		$frontend = new \ContentControl\Frontend();
+
+		foreach ( $test_blocks_html as $i => $block_content ) {
+			$this->assertSame( $expected_blocks_html[ $i ], $frontend->render_block( $block_content, $test_block ) );
+		}
+	}
 }
