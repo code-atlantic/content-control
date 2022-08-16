9d799653ab1de8605c37d182a34f471330d8dca7
Daniel Iser <daniel@code-atlantic.com>
2022-08-16 04:20:22 +0000

    Add controller base class

---
+++ classes/Base/Controller.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin controller.
+ *
+ * @copyright (c) 2021, Code Atlantic LLC.
+ *
+ * @package ContentControl
+ */
+
+namespace ContentControl\Base;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Localized container class.
+ */
+abstract class Controller implements \ContentControl\Interfaces\Controller {
+
+	/**
+	 * Plugin Container.
+	 *
+	 * @var \ContentControl\Core\Plugin;
+	 */
+	public $container;
+
+	/**
+	 * Initialize based on dependency injection principles.
+	 *
+	 * @param \ContentControl\Core\Plugin $container Plugin container.
+	 * @return void
+	 */
+	public function __construct( $container ) {
+		$this->container = $container;
+	}
+
+	/**
+	 * Initialize the controllers actions/ilters.
+	 *
+	 * @return void
+	 */
+	abstract public function init();
+
+}
