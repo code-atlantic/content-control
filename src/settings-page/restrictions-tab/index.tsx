29a496e800665a7a3f5345c87d5f2d0d6884a600
Daniel Iser <daniel@code-atlantic.com>
2022-08-19 02:48:23 +0000

    Mock list view

--- src/settings-page/restrictions-tab/index.tsx
+++ src/settings-page/restrictions-tab/index.tsx
@@ -1,5 +1,35 @@
+import { __ } from '@wordpress/i18n';
+import ListView from '@components/list-view';
+
 const RestrictionsTab = () => {
-	return <>Restrictions Placeholder</>;
+	const items = [
+		{
+			id: 1,
+			label: 'Restriction Set 1',
+		},
+		{
+			id: 2,
+			label: 'Restriction Set 2',
+		},
+		{
+			id: 3,
+			label: 'Restriction Set 3',
+		},
+		{
+			id: 4,
+			label: 'Restriction Set 4',
+		},
+	];
+
+	return (
+		<>
+			<ListView
+				items={ items }
+				columns={ { label: __( 'Label' ) } }
+				sortableColumns={ [ 'label' ] }
+			/>
+		</>
+	);
 };
 
 export default RestrictionsTab;
