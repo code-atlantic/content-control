68c6d726e11b3249d200bc0361b8d48c1279f293
Daniel Iser <daniel@code-atlantic.com>
2022-08-20 18:39:50 +0000

    Further refine list-table component

---
+++ src/components/list-table/index.tsx
@@ -0,0 +1,273 @@
+import classNames, { Argument } from 'classnames';
+import { __ } from '@wordpress/i18n';
+import { useState } from '@wordpress/element';
+import { chevronUp, chevronDown } from '@wordpress/icons';
+import { Button, CheckboxControl, Icon } from '@wordpress/components';
+
+import './editor.scss';
+
+type Item = {
+	id: number;
+	[ key: string ]: any;
+};
+
+type Props = {
+	items: Item[];
+	columns: {
+		[ key: string ]: React.ReactNode | ( () => React.ReactNode );
+	};
+	renderColumn: (
+		col: string,
+		item: Item
+	) => React.ReactNode | string | number;
+	sortableColumns: string[];
+	idCol?: string;
+	noItemsText?: string;
+	config?: {
+		showBulkSelect: boolean;
+	};
+	className?: Argument;
+};
+
+type CellProps = {
+	heading?: boolean;
+	children: React.ReactNode;
+	[ key: string ]: any;
+};
+
+const TableCell = ( { heading = false, children, ...props }: CellProps ) => {
+	return heading ? (
+		<th { ...props }>{ children }</th>
+	) : (
+		<td { ...props }>{ children }</td>
+	);
+};
+
+const ListTable = ( {
+	items,
+	columns,
+	sortableColumns = [],
+	idCol = 'id',
+	renderColumn: renderCell = ( col, item ) => item[ col ],
+	noItemsText = __( 'No items found.', 'content-control' ),
+	config = {
+		showBulkSelect: true,
+	},
+	className,
+}: Props ) => {
+	const { showBulkSelect } = config;
+
+	const cols = { [ idCol ]: columns[ idCol ] ?? '', ...columns };
+	const colCount = Object.keys( cols ).length;
+
+	const [ selectedItems, setSelectedItems ] = useState< number[] >( [] );
+	const [ selectAll, setSelectAll ] = useState< boolean >( false );
+	const [ sortBy, setSortBy ] = useState< string | null >(
+		sortableColumns.length ? sortableColumns[ 0 ] : null
+	);
+	const [ sortDirection, setSortDirection ] = useState< 'ASC' | 'DESC' >(
+		'ASC'
+	);
+
+	const sortedItems = ! sortBy
+		? items
+		: items.sort( ( a, b ) => {
+				if ( sortDirection === 'ASC' ) {
+					return a[ sortBy ] > b[ sortBy ] ? 1 : -1;
+				} else {
+					return b[ sortBy ] > a[ sortBy ] ? 1 : -1;
+				}
+		  } );
+
+	const ColumnHeaders = ( { header = false } ) => (
+		<>
+			{ Object.entries( cols ).map( ( [ col, colLabel ] ) => {
+				const isIdCol = col === idCol;
+				const isSortable = sortableColumns.indexOf( col ) >= 0;
+				const isSortedBy = col === sortBy;
+
+				const isBulkSelect = showBulkSelect && isIdCol;
+
+				const cellProps = {
+					key: col,
+					heading: ! isBulkSelect,
+					id: header && ! isBulkSelect ? col : undefined,
+					scope: ! isBulkSelect ? 'col' : undefined,
+					className: classNames( [
+						`column-${ col }`,
+						...( ! isBulkSelect && isSortable
+							? [ 'sortable', sortDirection.toLowerCase() ]
+							: [] ),
+						isBulkSelect && 'check-column',
+					] ),
+				};
+
+				const Label = () => (
+					<>
+						{ typeof colLabel === 'function' ? (
+							colLabel()
+						) : (
+							<>
+								<span>{ colLabel }</span>
+								{ isSortedBy && (
+									<span>
+										<Icon
+											icon={
+												sortDirection === 'ASC'
+													? chevronUp
+													: chevronDown
+											}
+											size={ 20 }
+										/>
+									</span>
+								) }
+							</>
+						) }
+					</>
+				);
+
+				return (
+					<TableCell { ...cellProps }>
+						{ isBulkSelect ? (
+							<CheckboxControl
+								onChange={ ( checked ) => {
+									setSelectAll( checked );
+									setSelectedItems(
+										! checked
+											? []
+											: items.map( ( item ) => item.id )
+									);
+								} }
+								checked={ selectAll }
+							/>
+						) : isSortable ? (
+							<Button
+								variant="link"
+								onClick={ () => {
+									if ( sortBy === col ) {
+										setSortDirection(
+											sortDirection === 'ASC'
+												? 'DESC'
+												: 'ASC'
+										);
+									} else {
+										setSortBy( col );
+										setSortDirection( 'ASC' );
+									}
+								} }
+							>
+								<Label />
+							</Button>
+						) : (
+							<Label />
+						) }
+					</TableCell>
+				);
+			} ) }
+		</>
+	);
+
+	return (
+		<table
+			className={ classNames( [
+				className,
+				'component-list-table',
+				'list-table',
+				sortedItems.length === 0 && 'no-items',
+			] ) }
+		>
+			<thead>
+				<tr>
+					<ColumnHeaders header={ true } />
+				</tr>
+			</thead>
+			<tbody>
+				{ sortedItems.length ? (
+					sortedItems.map( ( item ) => (
+						<tr key={ item.id }>
+							{ Object.entries( cols ).map(
+								( [ col, colLabel ] ) => {
+									const isIdCol = col === idCol;
+									const isBulkSelect =
+										isIdCol && showBulkSelect;
+
+									return (
+										<TableCell
+											key={ col }
+											heading={ isIdCol }
+											className={ classNames( [
+												`column-${ col }`,
+												showBulkSelect &&
+													isIdCol &&
+													'check-column',
+											] ) }
+											scope={
+												isIdCol ? 'row' : undefined
+											}
+											data-colname={
+												! isBulkSelect
+													? colLabel
+													: undefined
+											}
+										>
+											{ isIdCol ? (
+												<CheckboxControl
+													onChange={ ( checked ) => {
+														const newSelectedItems =
+															! checked
+																? selectedItems.filter(
+																		(
+																			id
+																		) =>
+																			id !==
+																			item.id
+																  )
+																: [
+																		...selectedItems,
+																		item.id,
+																  ];
+
+														setSelectedItems(
+															newSelectedItems
+														);
+
+														if (
+															items.length ===
+															newSelectedItems.length
+														) {
+															setSelectAll(
+																true
+															);
+														}
+													} }
+													checked={
+														selectedItems.indexOf(
+															item.id
+														) >= 0
+													}
+												/>
+											) : (
+												renderCell( col, item )
+											) }
+										</TableCell>
+									);
+								}
+							) }
+						</tr>
+					) )
+				) : (
+					<tr>
+						<td colSpan={ colCount }>{ noItemsText }</td>
+					</tr>
+				) }
+			</tbody>
+			<tfoot>
+				<tr>
+					<ColumnHeaders />
+				</tr>
+			</tfoot>
+		</table>
+	);
+};
+
+export default ListTable;
--- src/settings-page/restrictions-tab/index.tsx
+++ src/settings-page/restrictions-tab/index.tsx
@@ -1,32 +1,105 @@
 import { __ } from '@wordpress/i18n';
-import ListView from '@components/list-view';
+import { useState, useEffect } from '@wordpress/element';
+
+import TableView from '@components/table-view';
+import { Button, Icon } from '@wordpress/components';
+import { chevronUp } from '@wordpress/icons';
+
+const { restUrl, restBase } = contentControlSettingsPageVars;
 
 const RestrictionsTab = () => {
-	const items = [
-		{
-			id: 1,
-			label: 'Restriction Set 1',
-		},
-		{
-			id: 2,
-			label: 'Restriction Set 2',
-		},
-		{
-			id: 3,
-			label: 'Restriction Set 3',
-		},
-		{
-			id: 4,
-			label: 'Restriction Set 4',
-		},
-	];
+	const [ status, setStatus ] = useState( 'idle' );
+	const [ items, setItems ] = useState( [] );
+	const [ lastId, setLastId ] = useState( 0 );
+
+	const [ editRestriction, setEditRestriction ] = useState< number | null >(
+		null
+	);
+
+	useEffect( () => {
+		// Generic fetch function to retrieve settings and variables on render.
+		async function fetchData(
+			route: string,
+			setData: ( data: any ) => void
+		) {
+			setStatus( 'fetching' );
+
+			// blockVisibilityRestUrl is provided by wp_add_inline_script.
+			const fetchUrl = `${ restUrl }${ restBase }/${ route }`; // eslint-disable-line
+			const response = await fetch( fetchUrl, { method: 'GET' } ); // eslint-disable-line
+
+			if ( response.ok ) {
+				const data = await response.json();
+				setData( data );
+				setStatus( 'fetched' );
+			} else {
+				setStatus( 'error' );
+			}
+		}
+
+		fetchData( 'settings', ( { restrictions } ) =>
+			setItems(
+				restrictions
+					.sort( ( a, b ) => {
+						if (
+							undefined === a?.index &&
+							undefined === b?.index
+						) {
+							return 0;
+						}
+
+						if ( a?.index > b?.index ) {
+							return 1;
+						} else if ( a?.index < b?.index ) {
+							-1;
+						}
+
+						return 0;
+					} )
+					.map( ( item, id ) => {
+						if ( item.id ) {
+							return item;
+						}
+
+						return {
+							...item,
+							id,
+						};
+					} )
+			)
+		);
+	}, [] );
+
+	console.log( items );
 
 	return (
 		<>
-			<ListView
+			<TableView
 				items={ items }
-				columns={ { label: __( 'Label' ) } }
-				sortableColumns={ [ 'label' ] }
+				columns={ {
+					title: __( 'Title', 'content-control' ),
+					who: __( 'Who', 'content-control' ),
+				} }
+				sortableColumns={ [ 'title', 'who' ] }
+				renderColumn={ ( col, item ) => {
+					switch ( col ) {
+						case 'title':
+							return (
+								<Button
+									variant="link"
+									onClick={ () =>
+										setEditRestriction( item.id )
+									}
+								>
+									{ item[ col ] }
+								</Button>
+							);
+						default:
+							return item[ col ];
+					}
+				} }
+				// className="wp-list-table widefat fixed striped"
+				className="striped"
 			/>
 		</>
 	);
