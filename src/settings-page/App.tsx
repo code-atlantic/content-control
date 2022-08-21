f61ce6c13b28da5945c54c98a93a95f85abaeb9e
Daniel Iser <daniel@code-atlantic.com>
2022-08-21 08:23:04 +0000

    Remove unused tab props

--- src/settings-page/App.tsx
+++ src/settings-page/App.tsx
@@ -20,22 +20,37 @@ const App = () => {
 			name: 'restrictions',
 			title: __( 'Restrictions', 'content-control' ),
 			className: 'restrictions',
-			pageTitle: __( 'Content Control - Global Restrictions', 'content-control' ),
-			heading: __( 'Content Control - Global Restrictions', 'content-control' ),
+			pageTitle: __(
+				'Content Control - Global Restrictions',
+				'content-control'
+			),
+			heading: __(
+				'Content Control - Global Restrictions',
+				'content-control'
+			),
 			comp: <RestrictionsTab />,
 		},
 		{
 			name: 'settings',
 			title: __( 'Settings', 'content-control' ),
-			pageTitle: __( 'Content Control - Plugin Settings', 'content-control' ),
+			pageTitle: __(
+				'Content Control - Plugin Settings',
+				'content-control'
+			),
 			heading: __( 'Plugin Settings', 'content-control' ),
 			comp: <SettingsTab />,
 		},
 		{
 			name: 'upgrade',
 			title: __( 'Upgrade to Pro', 'content-control' ),
-			pageTitle: __( 'Content Control - Upgrade to Pro', 'content-control' ),
-			heading: __( 'Content Control - Upgrade to Pro', 'content-control' ),
+			pageTitle: __(
+				'Content Control - Upgrade to Pro',
+				'content-control'
+			),
+			heading: __(
+				'Content Control - Upgrade to Pro',
+				'content-control'
+			),
 			comp: <UpgradeTab />,
 		},
 	] ) as TabComponent[];
