1be9d4a8ad3039d26e65ea640c7d4eab70fc5f16
Daniel Iser <daniel@code-atlantic.com>
2022-08-21 08:29:30 +0000

    Reorder & add comments to

--- src/settings-page/restrictions-tab/index.tsx
+++ src/settings-page/restrictions-tab/index.tsx
@@ -1,16 +1,9 @@
+import { NumberParam, useQueryParam } from 'use-query-params';
+
 import { __ } from '@wordpress/i18n';
 import { blockMeta } from '@wordpress/icons';
 import { useState, useEffect } from '@wordpress/element';
-import {
-	Button,
-	Icon,
-	Modal,
-	Flex,
-	FlexItem,
-	__experimentalConfirmDialog as ConfirmDialog,
-	TextControl,
-	Notice,
-} from '@wordpress/components';
+import { __experimentalConfirmDialog as ConfirmDialog } from '@wordpress/components';
 
 import { getData, sendData } from './api';
 import Edit from './edit';
@@ -19,7 +12,6 @@ import List from './list';
 const RestrictionsTab = () => {
 	const [ status, setStatus ] = useState( 'idle' );
 	const [ restrictions, setRestrictions ] = useState< Restriction[] >( [] );
-	const [ lastId, setLastId ] = useState( 0 );
 	const [ idToDelete, setIdToDelete ] = useState< number | null >( null );
 
 	// Manage current set id for the editor via the URL.
@@ -28,58 +20,10 @@ const RestrictionsTab = () => {
 		NumberParam
 	);
 
+	/** Trigger async saving via effect by setting status to saving. */
 	const saveRestrictions = () => setStatus( 'saving' );
 
-	const trashRestriction = ( id: number ) => {
-		setRestrictions( [
-			...restrictions.filter( ( restriction ) => restriction.id !== id ),
-		] );
-		saveRestrictions();
-	};
-
-	const isSetValid = () => {
-		return (
-			currentSet &&
-			[ currentSet.title.length > 0 ].indexOf( false ) === -1
-		);
-	};
-
-	useEffect( () => {
-		getData(
-			'settings',
-			( { restrictions }: { restrictions: Restriction[] } ) =>
-				setRestrictions(
-					restrictions
-					// .sort( ( a, b ) => {
-					// 	if (
-					// 		undefined === a?.index &&
-					// 		undefined === b?.index
-					// 	) {
-					// 		return 0;
-					// 	}
-
-					// 	if ( a?.index > b?.index ) {
-					// 		return 1;
-					// 	} else if ( a?.index < b?.index ) {
-					// 		-1;
-					// 	}
-
-					// 	return 0;
-					// } )
-					// add id from array index if not set.
-					// .map( ( r: Restriction, id: number ) =>
-					// 	r.id
-					// 		? r
-					// 		: {
-					// 				...r,
-					// 				id,
-					// 		  }
-					// )
-				),
-			setStatus
-		);
-	}, [] );
-
+	// This effect hanldes status changes and async saving. */
 	useEffect( () => {
 		if ( 'saving' === status ) {
 			sendData( 'settings', { restrictions }, () => {
@@ -94,160 +38,95 @@ const RestrictionsTab = () => {
 		}
 	}, [ status ] );
 
-	/** Confirmation dialogue component. */
-	const ConfirmAndDelete = () => {
-		const restriction = restrictions.find( ( r ) => r.id === idToDelete );
+	/** Get a single restriction set by id. */
+	const getSet = ( id: number ) => restrictions.find( ( r ) => r.id === id );
 
-		if ( idToDelete === null || ! restriction ) {
-			return <></>;
-		}
-
-		return (
-			<ConfirmDialog
-				onCancel={ () => setIdToDelete( null ) }
-				onConfirm={ () => {
-					trashRestriction( idToDelete );
-					setIdToDelete( null );
-				} }
-			>
-				<p>
-					{ __(
-						'Are you sure you want to delete this set?',
-						'content-control'
-					) }
-				</p>
-				<p>{ restriction.title }</p>
-			</ConfirmDialog>
-		);
+	/** Trash a restriction set by id. */
+	const trashRestriction = ( id: number ) => {
+		setRestrictions( [
+			...restrictions.filter( ( restriction ) => restriction.id !== id ),
+		] );
+		saveRestrictions();
 	};
 
-	const EditRestriction = () =>
-		currentSet && (
-			<Modal
-				title={ __( 'Restriction Editor', 'content-control' ) }
-				onRequestClose={ () => updateCurrentSet( null ) }
-				shouldCloseOnClickOutside={ false }
-				style={ { width: '760px' } }
-			>
-				<Flex
-					style={ {
-						marginBottom: 20,
-					} }
-				>
-					<FlexItem
-						style={ {
-							flexGrow: 1,
-							maxWidth: 60,
-						} }
-					>
-						<div
-							style={ {
-								backgroundColor: '#e6f2f9',
-								borderRadius: 100,
-								width: 50,
-								height: 50,
-								padding: 10,
-								paddingLeft: 7,
-								paddingTop: 11,
-								verticalAlign: 'middle',
-								textAlign: 'center',
-							} }
-						>
-							<Icon icon={ blockMeta } size={ 30 } />
-						</div>
-					</FlexItem>
+	useEffect( () => {
+		getData(
+			'settings',
+			( { restrictions }: { restrictions: Restriction[] } ) =>
+				setRestrictions(
+					restrictions
+						.sort( ( a, b ) => {
+							if (
+								undefined === a?.index &&
+								undefined === b?.index
+							) {
+								return 0;
+							}
 
-					<FlexItem
-						style={ {
-							flexBasis: 'auto',
-							flexGrow: 3,
-						} }
-					>
-						<h3
-							style={ {
-								margin: 0,
-								marginBottom: 5,
-							} }
-						>
-							{ __( 'Conditional Logic', 'content-control ' ) }
-						</h3>
-						<p
-							style={ {
-								margin: 0,
-							} }
-						>
-							{ __(
-								'Use the power of conditional logic to control when a block is visible.',
-								'content-control'
-							) }
-						</p>
-					</FlexItem>
-				</Flex>
+							if ( a?.index > b?.index ) {
+								return 1;
+							} else if ( a?.index < b?.index ) {
+								-1;
+							}
 
-				<TextControl
-					label={ __( 'Condition set label', 'content-control' ) }
-					hideLabelFromVision={ true }
-					placeholder={ __(
-						'Condition set label',
-						'content-control'
-					) }
-					value={ currentSet.title }
-					onChange={ ( label ) =>
-						updateCurrentSet( {
-							...currentSet,
-							label,
+							return 0;
 						} )
-					}
-				/>
-
-				{ currentSet.title.length <= 0 && (
-					<Notice status="warning" isDismissible={ false }>
-						{ __(
-							'Enter a label for this set.',
-							'content-control'
-						) }
-					</Notice>
-				) }
-
-				<Flex justify="right">
-					<FlexItem>
-						<Button onClick={ () => updateCurrentSet( null ) }>
-							{ __( 'Cancel', 'content-control' ) }
-						</Button>
-					</FlexItem>
-					<FlexItem>
-						<Button
-							disabled={ ! isSetValid() }
-							variant="primary"
-							onClick={ () => {
-								if ( ! isSetValid() ) {
-									return;
-								}
-								updateSet( currentSet );
-								updateCurrentSet( null );
-							} }
-						>
-							{ __( 'Save', 'content-control' ) }
-						</Button>
-					</FlexItem>
-				</Flex>
-			</Modal>
+						//add id from array index if not set.
+						.map( ( r: Restriction, id: number ) =>
+							r.id
+								? r
+								: {
+										...r,
+										id,
+								  }
+						)
+				),
+			setStatus
 		);
+	}, [] );
+
+	const setToEdit = null !== idToEdit ? getSet( idToEdit ) : null;
 
 	return (
 		<>
 			<List
 				restrictions={ restrictions }
-				editSet={ ( restriction ) => updateCurrentSet( restriction ) }
+				editSet={ ( restriction ) => setIdToEdit( restriction.id ) }
 				deleteSet={ ( id ) => setIdToDelete( id ) }
 				isDeleting={ !! idToDelete }
 			/>
 
-			{ currentSet && (
-				<Edit values={ currentSet } onChange={ updateCurrentSet } />
+			{ setToEdit && (
+				<Edit
+					values={ setToEdit }
+					onSave={ ( newValues ) =>
+						setRestrictions( [
+							...restrictions.map( ( r ) =>
+								r.id === idToEdit ? newValues : r
+							),
+						] )
+					}
+					onClose={ () => setIdToEdit( null ) }
+				/>
 			) }
 
-			<ConfirmAndDelete />
+			{ null !== idToDelete && (
+				<ConfirmDialog
+					onCancel={ () => setIdToDelete( null ) }
+					onConfirm={ () => {
+						trashRestriction( idToDelete );
+						setIdToDelete( null );
+					} }
+				>
+					<p>
+						{ __(
+							'Are you sure you want to delete this set?',
+							'content-control'
+						) }
+					</p>
+					<p>{ getSet( idToDelete )?.title }</p>
+				</ConfirmDialog>
+			) }
 		</>
 	);
 };
