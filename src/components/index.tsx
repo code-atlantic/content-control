99eb173fa8f424220b8a22011897986d36074c20
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 07:50:52 +0000

    Export defaults from main index

---
+++ src/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as ControlledTabPanel } from './controlled-tab-panel';
+export { default as ListTable } from './list-table';
+export { default as RadioButtonControl } from './radio-buttons-control';
+export { default as SearchableMulticheckControl } from './searchable-multicheck-control';
