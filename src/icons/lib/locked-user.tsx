9fb7e906f2c6a8460c38b31dda6164729615d935
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 04:08:31 +0000

    Fix malformatted parameters

--- src/icons/lib/locked-user.tsx
+++ src/icons/lib/locked-user.tsx
@@ -5,30 +5,30 @@ import { SVG, Path, G, Defs, Rect } from '@wordpress/primitives';
 
 const lockedUser = (
 	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 24" fill="none">
-		<G clip-path="url(#clip0_203_600)">
+		<G clipPath="url(#clip0_203_600)">
 			<Path
 				d="M11.5 13C8.676 13 6.171 13.638 4.525 14.193C3.31 14.604 2.5 15.749 2.5 17.032V21C2.5 21 10.458 21 10.5 21"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<Path
 				d="M23.5 18H15.5V23H23.5V18Z"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<Path
 				d="M17.5 18V15C17.5 13.9 18.4 13 19.5 13C20.6 13 21.5 13.9 21.5 15V18"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 			<Path
 				d="M11.5 13C8.739 13 6.5 9.761 6.5 7V6C6.5 3.239 8.739 1 11.5 1C14.261 1 16.5 3.239 16.5 6V7C16.5 9.761 14.261 13 11.5 13Z"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
 			/>
 		</G>
 		<Defs>
@@ -40,7 +40,7 @@ const lockedUser = (
 					transform="translate(0.5)"
 				/>
 			</clipPath>
-		</Defs>{ ' ' }
+		</Defs>
 	</SVG>
 );
 
