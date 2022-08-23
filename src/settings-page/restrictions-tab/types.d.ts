3af369d50cf43e13ea72df24fd5100b28372597f
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 07:11:40 +0000

    Fill out restriction typing

--- src/settings-page/restrictions-tab/types.d.ts
+++ src/settings-page/restrictions-tab/types.d.ts
@@ -2,5 +2,11 @@ interface Restriction extends TableItemBase {
 	title: string;
 	who: 'logged_in' | 'logged_out';
 	roles: string[];
+	protectionMethod: 'redirect' | 'message';
+	redirectType: 'login' | 'home' | 'custom';
+	redirectUrl: string;
+	showExcerpts: boolean;
+	overrideMessage: boolean;
+	customMessage: string;
 	[ key: string ]: any;
 }
