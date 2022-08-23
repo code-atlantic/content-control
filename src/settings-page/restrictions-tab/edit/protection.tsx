1db9e5d8bc69d7dbd44d1d4e3a2e2dd33ec4cb3e
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 07:48:11 +0000

    Added rest of fields to protection tab

--- src/settings-page/restrictions-tab/edit/protection.tsx
+++ src/settings-page/restrictions-tab/edit/protection.tsx
@@ -1,26 +1,105 @@
+/* WordPress Imports */
 import { __ } from '@wordpress/i18n';
-import { TextControl, Notice } from '@wordpress/components';
+import {
+	CheckboxControl,
+	TextareaControl,
+	TextControl,
+} from '@wordpress/components';
 
-type Props = ContentControl.Settings.Restrictions.EditTabProps;
+/* Internal Imports */
+import { RadioButtonControl } from '@components';
+import { protectionMethodOptions, redirectTypeOptions } from '../options';
 
-const ProtectionTab = ( { values, onChange, updateValue }: Props ) => {
+/* Type Imports */
+import type { EditTabProps } from '.';
 
+const ProtectionTab = ( { values, updateValues }: EditTabProps ) => {
 	return (
-		<>
-			<TextControl
-				label={ __( 'Restriction label', 'content-control' ) }
-				hideLabelFromVision={ true }
-				placeholder={ __( 'Condition set label', 'content-control' ) }
-				value={ values.title }
-				onChange={ ( title ) => updateValue( 'title', title ) }
+		<div className="protection-tab">
+			<h3>{ __( 'Protecting Content', 'content-control' ) }</h3>
+			<p>
+				{ __(
+					'When a user does not have access, the following options help control their experience.',
+					'content-control'
+				) }
+			</p>
+
+			<RadioButtonControl< Restriction[ 'protectionMethod' ] >
+				label={ __( 'Who can see this content?', 'content-control' ) }
+				value={ values.protectionMethod }
+				onChange={ ( protectionMethod ) =>
+					updateValues( { protectionMethod } )
+				}
+				options={ protectionMethodOptions }
 			/>
 
-			{ values.title.length <= 0 && (
-				<Notice status="warning" isDismissible={ false }>
-					{ __( 'Enter a label for this set.', 'content-control' ) }
-				</Notice>
+			{ 'redirect' === values.protectionMethod && (
+				<>
+					<RadioButtonControl< Restriction[ 'redirectType' ] >
+						label={ __(
+							'Where will the user be taken?',
+							'content-control'
+						) }
+						value={ values.redirectType }
+						onChange={ ( redirectType ) =>
+							updateValues( { redirectType } )
+						}
+						options={ redirectTypeOptions }
+					/>
+
+					{ 'custom' === values.redirectType && (
+						<TextControl
+							label={ __(
+								'Custom Redirect URL',
+								'content-control'
+							) }
+							value={ values.redirectUrl }
+							onChange={ ( redirectUrl ) =>
+								updateValues( { redirectUrl } )
+							}
+						/>
+					) }
+				</>
 			) }
-		</>
+
+			{ 'message' === values.protectionMethod && (
+				<>
+					<CheckboxControl
+						label={ __(
+							'Show excerpts above access denied message?',
+							'content-control'
+						) }
+						checked={ values.showExcerpts }
+						onChange={ ( showExcerpts ) =>
+							updateValues( { showExcerpts } )
+						}
+					/>
+					<CheckboxControl
+						label={ __(
+							'Override the default message?',
+							'content-control'
+						) }
+						checked={ values.overrideMessage }
+						onChange={ ( overrideMessage ) =>
+							updateValues( { overrideMessage } )
+						}
+					/>
+
+					{ values.overrideMessage && (
+						<TextareaControl
+							label={ __(
+								'Enter a custom message to display to restricted users',
+								'content-control'
+							) }
+							value={ values.customMessage }
+							onChange={ ( customMessage ) =>
+								updateValues( { customMessage } )
+							}
+						/>
+					) }
+				</>
+			) }
+		</div>
 	);
 };
 
