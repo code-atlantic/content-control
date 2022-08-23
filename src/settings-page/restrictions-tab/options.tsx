eda9996c52c5e32fce45b48131f6e5b901ca1ee4
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 01:04:41 +0000

    Add icons to button options

--- src/settings-page/restrictions-tab/options.tsx
+++ src/settings-page/restrictions-tab/options.tsx
@@ -1,6 +1,18 @@
+import { Icon } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { incognito, lockedUser } from '@icons';
 
-export const whoOptions: { [ k in Restriction[ 'who' ] ]: string } = {
-	logged_in: __( 'Logged In Users', 'content-control' ),
-	logged_out: __( 'Logged Out Users', 'content-control' ),
+export const whoOptions: { [ k in Restriction[ 'who' ] ]: JSX.Element } = {
+	logged_in: (
+		<>
+			<Icon icon={ incognito } size={ 18 } />
+			{ __( 'Logged In Users', 'content-control' ) }
+		</>
+	),
+	logged_out: (
+		<>
+			<Icon icon={ lockedUser } size={ 18 } />
+			{ __( 'Logged Out Users', 'content-control' ) }
+		</>
+	),
 };
