9bba0e42427f35ff5f89b46efa80752db6e3a520
Daniel Iser <daniel@code-atlantic.com>
2022-08-23 07:11:13 +0000

    Add protection options

--- src/settings-page/restrictions-tab/options.tsx
+++ src/settings-page/restrictions-tab/options.tsx
@@ -1,18 +1,73 @@
-import { Icon } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
-import { incognito, lockedUser } from '@icons';
+import { home, link, login } from '@wordpress/icons';
 
-export const whoOptions: { [ k in Restriction[ 'who' ] ]: JSX.Element } = {
-	logged_in: (
-		<>
-			<Icon icon={ incognito } size={ 18 } />
-			{ __( 'Logged In Users', 'content-control' ) }
-		</>
-	),
-	logged_out: (
-		<>
-			<Icon icon={ lockedUser } size={ 18 } />
-			{ __( 'Logged Out Users', 'content-control' ) }
-		</>
-	),
-};
+import {
+	incognito,
+	lockedUser,
+	protectedMessage,
+	protectedRedirect,
+} from '@icons';
+
+export const whoOptions: {
+	value: Restriction[ 'who' ];
+	label: string;
+	[ key: string ]: any;
+}[] = [
+	{
+		value: 'logged_in',
+		label: __( 'Logged In Users', 'content-control' ),
+		icon: incognito,
+		iconSize: 18,
+	},
+	{
+		value: 'logged_out',
+		label: __( 'Logged Out Users', 'content-control' ),
+		icon: lockedUser,
+		iconSize: 18,
+	},
+];
+
+export const protectionMethodOptions: {
+	value: Restriction[ 'protectionMethod' ];
+	label: string;
+	[ key: string ]: any;
+}[] = [
+	{
+		value: 'redirect',
+		label: __( 'Redirect', 'content-control' ),
+		icon: protectedRedirect,
+		iconSize: 18,
+	},
+	{
+		value: 'message',
+		label: __( 'Custom Message', 'content-control' ),
+		icon: protectedMessage,
+		iconSize: 18,
+	},
+];
+
+export const redirectTypeOptions: {
+	value: Restriction[ 'redirectType' ];
+	label: string;
+	[ key: string ]: any;
+}[] = [
+	{
+		value: 'login',
+		label: __( 'Login & Back', 'content-control' ),
+		icon: login,
+		iconSize: 18,
+	},
+	{
+		value: 'home',
+		label: __( 'Home Page', 'content-control' ),
+		icon: home,
+		iconSize: 18,
+	},
+
+	{
+		value: 'custom',
+		label: __( 'Custom URL', 'content-control' ),
+		icon: link,
+		iconSize: 18,
+	},
+];
