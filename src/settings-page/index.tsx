da0b5b3ccf723f34020a8aaaf54dffc7cbb585a4
Daniel Iser <daniel@code-atlantic.com>
2022-08-25 23:08:10 +0000

    Add RegistryProvider for @wordpress/data & useSelect

--- src/settings-page/index.tsx
+++ src/settings-page/index.tsx
@@ -3,6 +3,8 @@ import { render, StrictMode } from '@wordpress/element';
 import { QueryParamProvider } from 'use-query-params';
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
 import { BrowserRouter } from 'react-router-dom';
+import { createRegistry, RegistryProvider } from '@wordpress/data';
+import { store as coreStore } from '@wordpress/core-data';
 
 import App from './App';
 
@@ -16,12 +18,17 @@ declare global {
 	};
 }
 
+const registry = createRegistry( {} );
+registry.register( coreStore );
+
 domReady( () => {
 	render(
 		<StrictMode>
 			<BrowserRouter>
 				<QueryParamProvider adapter={ ReactRouter6Adapter }>
-					<App />
+					<RegistryProvider value={ registry }>
+						<App />
+					</RegistryProvider>
 				</QueryParamProvider>
 			</BrowserRouter>
 		</StrictMode>,
