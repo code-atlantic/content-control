7cd6225bb699d904ae5f81ba4421c922406ad11a
Daniel Iser <daniel@code-atlantic.com>
2022-08-28 08:47:02 +0000

    Add store types

--- src/data/restrictions/types.d.ts
+++ src/data/restrictions/types.d.ts
@@ -1,3 +1,9 @@
 type RestrictionsState = {
 	restrictions: Restriction[];
 };
+
+interface RestrictionsStore {
+	State: RestrictionsState;
+	Selectors: typeof import('./selectors');
+	Actions: typeof import('./actions');
+}
--- src/data/settings/types.d.ts
+++ src/data/settings/types.d.ts
@@ -13,11 +13,8 @@ type SettingsState = {
 	settings: Settings;
 };
 
-// declare module '@types/wordpress' {
-// 	export function dispatch(
-// 		key: 'content-control/setttings'
-// 	): typeof import('./actions');
-// 	export function select(
-// 		key: 'content-control/setttings'
-// 	): typeof import('./selectors');
-// }
+interface SettingsStore {
+	State: SettingsState;
+	Selectors: typeof import('./selectors');
+	Actions: typeof import('./actions');
+}
