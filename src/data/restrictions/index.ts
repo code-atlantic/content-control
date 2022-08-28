482300a4f54f520d802111d493fce9f23cbab5e4
Daniel Iser <daniel@code-atlantic.com>
2022-08-28 08:46:53 +0000

    Fix whitespace

--- src/data/restrictions/index.ts
+++ src/data/restrictions/index.ts
@@ -1,28 +1,28 @@
 /**
  * WordPress dependencies
  */
- import { createReduxStore } from '@wordpress/data';
- import { controls as wpControls } from '@wordpress/data-controls';
+import { createReduxStore } from '@wordpress/data';
+import { controls as wpControls } from '@wordpress/data-controls';
 
- import * as actions from './actions';
- import * as selectors from './selectors';
- import * as resolvers from './resolvers';
- import reducer from './reducer';
- import localControls from '../controls';
+import * as actions from './actions';
+import * as selectors from './selectors';
+import * as resolvers from './resolvers';
+import reducer from './reducer';
+import localControls from '../controls';
 
- import STORE_NAME from './name';
+import STORE_NAME from './name';
 
- const initialState: RestrictionsState = { restrictions: [] };
+const initialState: RestrictionsState = { restrictions: [] };
 
- const storeConfig = () => ( {
-	 initialState,
-	 selectors,
-	 actions,
-	 reducer,
-	 resolvers,
-	 controls: { ...wpControls, ...localControls },
- } );
+const storeConfig = () => ( {
+	initialState,
+	selectors,
+	actions,
+	reducer,
+	resolvers,
+	controls: { ...wpControls, ...localControls },
+} );
 
- const store = createReduxStore( STORE_NAME, storeConfig() );
+const store = createReduxStore( STORE_NAME, storeConfig() );
 
- export { STORE_NAME, store };
+export { STORE_NAME, store };
