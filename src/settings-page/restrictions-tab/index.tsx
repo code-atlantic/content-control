7e5d5621dc517613d890e203002322e86e89ea4b
Daniel Iser <daniel@code-atlantic.com>
2022-09-02 05:14:11 +0000

    Clean up and organize restrictions tab

--- src/settings-page/restrictions-tab/index.tsx
+++ src/settings-page/restrictions-tab/index.tsx
@@ -1,213 +1,110 @@
-import { NumberParam, useQueryParam } from 'use-query-params';
+/** External Imports */
+import { BooleanParam, NumberParam, useQueryParams } from 'use-query-params';
 
+/** WordPress Imports */
 import { __ } from '@wordpress/i18n';
-import { useState, useEffect, useRef } from '@wordpress/element';
-import {
-	Button,
-	Notice,
-	__experimentalConfirmDialog as ConfirmDialog,
-} from '@wordpress/components';
+import { Button } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useState, useEffect } from '@wordpress/element';
 
-import { getData, sendData } from './api';
+/** Internal Imports */
+import { restrictionsStore } from '@data';
 import Edit from './edit';
 import List from './list';
 
+/** Style Imports */
 import './editor.scss';
 
-import { restrictionDefaults } from '@data/restrictions';
-
+/**
+ * Generates the Restrictions tab component & sub-app.
+ *
+ * @returns Restrictions tab component.
+ */
 const RestrictionsTab = () => {
-	const [ status, setStatus ] = useState( 'loaded' );
-	const [ noticeMessage, setNoticeMessage ] = useState< string | null >(
-		null
-	);
-	const [ restrictions, setRestrictions ] = useState< Restriction[] >( [] );
+	// Fetch needed data from the @data & @wordpress/data stores.
+	const { isEditorActive, editorId } = useSelect( ( select ) => {
+		const storeSelect = select( restrictionsStore );
 
-	const nextIdRef = useRef< number >( 1 );
-
-	const updateNextIdRef = () => {
-		nextIdRef.current = restrictions.reduce(
-			( maxId, r ) => ( r.id > maxId ? ( maxId = r.id ) : maxId ),
-			1
-		);
-	};
-
-	useEffect( () => {
-		updateNextIdRef();
-	}, [ restrictions ] );
-
-	const nextId = () => {
-		nextIdRef.current += 1;
-		return nextIdRef.current;
-	};
-
-	const [ idToDelete, setIdToDelete ] = useState< number | null >( null );
-
-	// Manage current set id for the editor via the URL.
-	const [ idToEdit = null, setIdToEdit ] = useQueryParam(
-		'edit',
-		NumberParam
-	);
-
-	/** Trigger async saving via effect by setting status to saving. */
-	const saveRestrictions = ( newRestrictions: Restriction[] ) => {
-		setRestrictions( newRestrictions );
-		setStatus( 'saving' );
-	};
-
-	// This effect hanldes status changes and async saving. */
-	useEffect( () => {
-		if ( 'saving' === status ) {
-			sendData( 'settings', { restrictions }, () => {
-				setStatus( 'success' );
-				setNoticeMessage(
-					__( 'Succesfully saved restrictions', 'content-control' )
-				);
-			} );
-		}
-
-		if ( 'success' === status || 'error' === status ) {
-			setTimeout( () => {
-				setNoticeMessage( null );
-				setStatus( 'idle' );
-			}, 4000 );
-		}
-	}, [ status ] );
-
-	/** Get a single restriction set by id. */
-	const getSet = ( id: number ) => restrictions.find( ( r ) => r.id === id );
-
-	const updateSet = ( set: Restriction ) => {
-		const exists = getSet( set.id )?.id === set.id;
-
-		if ( exists ) {
-			// If set exists, map over all sets & replace the matching set.
-			saveRestrictions( [
-				...restrictions.map( ( r ) => ( r.id === set.id ? set : r ) ),
-			] );
-		} else {
-			saveRestrictions( [
-				...restrictions,
-				{
-					...set,
-					id: nextId(),
-				},
-			] );
-		}
-	};
-
-	/** Trash a restriction set by id. */
-	const trashRestriction = ( id: number ) =>
-		saveRestrictions( [
-			...restrictions.filter( ( restriction ) => restriction.id !== id ),
-		] );
-
-	useEffect( () => {
-		getData(
-			'settings',
-			( { restrictions = [] }: { restrictions: Restriction[] } ) =>
-				setRestrictions(
-					restrictions
-						.sort( ( a, b ) => {
-							if (
-								undefined === a?.index &&
-								undefined === b?.index
-							) {
-								return 0;
-							}
-
-							if ( a?.index > b?.index ) {
-								return 1;
-							} else if ( a?.index < b?.index ) {
-								-1;
-							}
-
-							return 0;
-						} )
-						//add id from array index if not set.
-						.map( ( r: Restriction, i: number ) =>
-							r.id
-								? r
-								: {
-										...r,
-										id: i + 1,
-								  }
-						)
-				),
-			setStatus
-		);
+		return {
+			// Editor Status.
+			editorId: storeSelect.getEditorId(),
+			isEditorActive: storeSelect.isEditorActive(),
+		};
 	}, [] );
 
-	const setToEdit = ( () => {
-		if ( null === idToEdit ) {
-			return null;
+	// Grab needed action dispatchers.
+	const { changeEditorId, deleteRestriction } =
+		useDispatch( restrictionsStore );
+
+	// Allow initiating the editor directly from a url.
+	const [ queryParams, setQueryParams ] = useQueryParams( {
+		edit: NumberParam,
+		add: BooleanParam,
+	} );
+
+	// Quick helper to reset all query params.
+	const clearEditorParams = () =>
+		setQueryParams( { add: undefined, edit: undefined } );
+
+	// Extract params with usable names.
+	const { edit: idToEdit, add: addNewSet } = queryParams;
+
+	// Clear params on component removal.
+	useEffect( () => () => clearEditorParams(), [] );
+
+	// Sync url param changes for editor ID to the editor.
+	useEffect( () => {
+		let urlId: EditorId = idToEdit && idToEdit > 0 ? idToEdit : undefined;
+
+		if ( addNewSet ) {
+			urlId = 'new';
 		}
 
-		// This prevents showing the editor until the restrictions have loaded.
-		if ( [ 'loaded', 'fetching', 'error' ].indexOf( status ) >= 0 ) {
-			return null;
+		if ( urlId !== editorId ) {
+			changeEditorId( urlId );
 		}
+	}, [ addNewSet, idToEdit ] );
 
-		const set = getSet( idToEdit );
-
-		if ( idToEdit > 0 ) {
-			if ( set?.id !== idToEdit ) {
-				setIdToEdit( null );
-				setNoticeMessage(
-					__( 'No matching set found', 'content-control' )
-				);
-				setStatus( 'error' );
-				return null;
-			}
-			return set;
+	// Sync editorId changes to the URL.
+	useEffect( () => {
+		if ( ! isEditorActive ) {
+			clearEditorParams();
 		}
+	}, [ isEditorActive ] );
 
-		return restrictionDefaults;
-	} )();
+	/**
+	 * Set the editor to edit a specific restriction.
+	 *
+	 * This both updates the editorId & sets matching url params.
+	 *
+	 * NOTE: It is important that both get updated at the same time, to prevent
+	 * infinite state updates via useEffect above.
+	 *
+	 * @param id Id to edit.
+	 */
+	const setEditorId = ( id: number | 'new' | undefined ) => {
+		setQueryParams( {
+			add: id === 'new' ? true : undefined,
+			edit: typeof id === 'number' && id > 0 ? id : undefined,
+		} );
+		changeEditorId( id );
+	};
 
 	return (
 		<div className="restriction-list">
-			<Button onClick={ () => setIdToEdit( -1 ) } variant="primary">
+			<Button onClick={ () => changeEditorId( 'new' ) } variant="primary">
 				{ __( 'Add New', 'content-control' ) }
 			</Button>
 
-			<List
-				restrictions={ restrictions }
-				editSet={ ( restriction ) => setIdToEdit( restriction.id ) }
-				deleteSet={ ( id ) => setIdToDelete( id ) }
-				isDeleting={ !! idToDelete }
-			/>
+			<hr />
 
-			{ noticeMessage && (
-				<Notice className={ `is-${ status }` } isDismissible={ false }>
-					{ noticeMessage }
-				</Notice>
-			) }
+			<List />
 
-			{ null !== setToEdit && (
+			{ isEditorActive && (
 				<Edit
-					values={ setToEdit }
-					onSave={ updateSet }
-					onClose={ () => setIdToEdit( null ) }
+					// onSave={ clearEditorParams }
+					onClose={ clearEditorParams }
 				/>
-			) }
-
-			{ null !== idToDelete && (
-				<ConfirmDialog
-					onCancel={ () => setIdToDelete( null ) }
-					onConfirm={ () => {
-						trashRestriction( idToDelete );
-						setIdToDelete( null );
-					} }
-				>
-					<p>
-						{ __(
-							'Are you sure you want to delete this set?',
-							'content-control'
-						) }
-					</p>
-					<p>{ getSet( idToDelete )?.title }</p>
-				</ConfirmDialog>
 			) }
 		</div>
 	);
