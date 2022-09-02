2ba61b74e5c39b92eebaf94de9bf2eecf5811541
Daniel Iser <daniel@code-atlantic.com>
2022-09-02 05:13:47 +0000

    Update editor to use data stores

--- src/settings-page/restrictions-tab/edit/index.tsx
+++ src/settings-page/restrictions-tab/edit/index.tsx
@@ -1,77 +1,145 @@
-import { useQueryParam, StringParam } from 'use-query-params';
+import { noop } from 'lodash';
+import { StringParam, withDefault, useQueryParam } from 'use-query-params';
 
-import { __ } from '@wordpress/i18n';
 import { link } from '@wordpress/icons';
-import { useState } from '@wordpress/element';
+import { __, sprintf } from '@wordpress/i18n';
+import { useEffect } from '@wordpress/element';
 import { applyFilters } from '@wordpress/hooks';
-import { Button, Modal, TabPanel } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { Button, Modal, Spinner, TabPanel } from '@wordpress/components';
 
+import { restrictionsStore } from '@data';
 import GeneralTab from './general';
 import ProtectionTab from './protection';
 import ContentTab from './content';
 
 import { documenationUrl } from '../../../config';
 
-import { restrictionDefaults } from '@data/restrictions';
-
 export type EditProps = {
-	values: Restriction;
-	onSave: ( values: Restriction ) => void;
-	onClose: () => void;
+	onSave?: ( values: Restriction ) => void;
+	onClose?: () => void;
 };
 
 export type EditTabProps = EditProps & {
-	onChange: ( values: Restriction ) => void;
-	updateValue: < K extends keyof Restriction >(
-		key: K,
-		newValue: Restriction[ K ]
-	) => void;
+	values: Restriction;
 	updateValues: ( values: Partial< Restriction > ) => void;
+	updateSettings: ( settings: Partial< Restriction[ 'settings' ] > ) => void;
 };
 
-const Edit = ( {
-	values: editorValues = restrictionDefaults,
-	onSave,
-	onClose,
-}: EditProps ) => {
-	const [ tab = 'general', changeTab ] = useQueryParam( 'tab', StringParam );
+const Edit = ( { onSave = noop, onClose = noop }: EditProps ) => {
+	// Fetch needed data from the @data & @wordpress/data stores.
+	const { editorId, values, isSaving } = useSelect(
+		( select ) => ( {
+			editorId: select( restrictionsStore ).getEditorId(),
+			values: select( restrictionsStore ).getEditorValues(),
+			isSaving: select( restrictionsStore ).isDispatching( [
+				'createRestriction',
+				'updateRestriction',
+			] ),
+		} ),
+		[]
+	);
 
-	const [ values, onChange ] =
-		useState< EditProps[ 'values' ] >( editorValues );
+	// Get action dispatchers.
+	const {
+		updateEditorValues: updateValues,
+		createRestriction,
+		updateRestriction,
+		clearEditorData,
+	} = useDispatch( restrictionsStore );
+
+	// Get the current editor tab.
+	const [ tab, changeTab ] = useQueryParam(
+		'tab',
+		withDefault( StringParam, 'general' )
+	);
+
+	// Clear params on component unmount.
+	useEffect( () => () => changeTab( undefined ), [] );
+
+	// When no editorId, dont' show the editor.
+	if ( ! editorId ) {
+		return <>{ __( 'Editor requires a valid id', 'content-control' ) }</>;
+	}
+
+	// When no values, dont' show the editor.
+	if ( ! values ) {
+		return (
+			<>
+				{ __(
+					'Editor requires a valid restriction.',
+					'content-control'
+				) }
+			</>
+		);
+	}
 
 	/**
-	 * Update a single fields value in the restriction set.
+	 * Trigger the correct save action.
 	 *
-	 * @param key Key of Restriction object property.
-	 * @param newValue Value of Restriction object property.
+	 * @returns Nothing
 	 */
-	const updateValue: EditTabProps[ 'updateValue' ] = ( key, newValue ) => {
-		onChange( { ...values, [ key ]: newValue } );
+	function saveRestriction() {
+		if ( ! editorId || ! values ) {
+			return;
+		}
+
+		const exists = editorId === 'new' ? false : editorId > 0;
+
+		if ( exists ) {
+			updateRestriction( values );
+		} else {
+			createRestriction( values );
+		}
+
+		onSave( values );
+	}
+
+	/**
+	 * Update settings for the given restriction.
+	 *
+	 * @param newSettings Updated settings.
+	 */
+	const updateSettings = (
+		newSettings: Partial< Restriction[ 'settings' ] >
+	) => {
+		updateValues( {
+			...values,
+			settings: {
+				...values?.settings,
+				...newSettings,
+			},
+		} );
 	};
 
-	const updateValues: EditTabProps[ 'updateValues' ] = ( newValues ) =>
-		onChange( { ...values, ...newValues } );
-
-	// Checks of the set values are valid.
+	/**
+	 * Checks of the set values are valid.
+	 *
+	 * @returns True when set values are valid.
+	 */
 	const isSetValid = () => {
 		return values && [ values.title.length > 0 ].indexOf( false ) === -1;
 	};
 
-	// Handles closing the editor and removing url params.
+	/**
+	 * Handles closing the editor and removing url params.
+	 */
 	const closeEditor = () => {
 		changeTab( undefined );
+		clearEditorData();
 		onClose();
 	};
 
+	// Define props passed to each child tab component.
 	const componentProps = {
 		values,
+		updateValues,
+		updateSettings,
 		onSave,
 		onClose,
-		onChange,
-		updateValue,
-		updateValues,
 	};
 
+	// Filtered & mappable list of TabComponent definitions.
 	const tabs: TabComponent[] = applyFilters(
 		'contentControl.restrictionEditorTabs',
 		[
@@ -93,9 +161,17 @@ const Edit = ( {
 		]
 	) as TabComponent[];
 
+	// Define the modal title dynamically using editorId.
+	const modalTitle = sprintf(
+		__( 'Restriction Editor%s', 'content-control' ),
+		editorId === 'new'
+			? ': ' + __( 'New Restriction', 'content-control' )
+			: `: #${ values.id } - ${ values.title }`
+	);
+
 	return (
 		<Modal
-			title={ __( 'Restriction Editor', 'content-control' ) }
+			title={ modalTitle }
 			className="restriction-editor"
 			onRequestClose={ () => closeEditor() }
 			shouldCloseOnClickOutside={ false }
@@ -125,23 +201,23 @@ const Edit = ( {
 					variant="tertiary"
 					isDestructive={ true }
 					onClick={ () => closeEditor() }
+					disabled={ ! isSetValid() || isSaving }
 				/>
 				<Button
-					text={
-						values.id > 0
-							? __( 'Save Restriction', 'content-control' )
-							: __( 'Add Restriction', 'content-control' )
-					}
 					variant="primary"
-					disabled={ ! isSetValid() }
+					disabled={ ! isSetValid() || isSaving }
 					onClick={ () => {
 						if ( ! isSetValid() ) {
 							return;
 						}
-						onSave( values );
-						closeEditor();
+						saveRestriction();
 					} }
-				/>
+				>
+					{ isSaving && <Spinner /> }
+					{ editorId === 'new'
+						? __( 'Add Restriction', 'content-control' )
+						: __( 'Save Restriction', 'content-control' ) }
+				</Button>
 			</div>
 		</Modal>
 	);
