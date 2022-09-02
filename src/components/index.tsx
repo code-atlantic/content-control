2521d63f171cfa6fc508ecb45a3f3c12bb34849f
Daniel Iser <daniel@code-atlantic.com>
2022-09-02 23:47:25 +0000

    Add base link control component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { default as ControlledTabPanel } from './controlled-tab-panel';
+export { default as LinkControl } from './link-control';
 export { default as ListTable } from './list-table';
 export { default as RadioButtonControl } from './radio-buttons-control';
 export { default as SearchableMulticheckControl } from './searchable-multicheck-control';
