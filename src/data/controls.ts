644ce3397ecd7ba8ecdfa81023bea30d9a740035
Daniel Iser <daniel@code-atlantic.com>
2022-09-03 09:50:56 +0000

    Abstract fetch creds to new function

--- src/data/controls.ts
+++ src/data/controls.ts
@@ -2,37 +2,50 @@ const { restBase } = contentControlSettingsPage;
 
 const restUrl = `${ wpApiSettings.root }${ restBase }/`;
 
-export const fetch = (
-	path: string,
-	options: { [ key: string ]: any } = {}
-) => {
-	options.headers = {
+type FetchOptions = { [ key: string ]: any };
+type FetchArgs = {
+	path: string;
+	options: { [ key: string ]: any };
+};
+
+const fetchCredOpts = ( options: FetchOptions = {} ): RequestInit => ( {
+	...options,
+	headers: {
+		...options.headers,
 		'Content-Type': 'application/json',
 		'X-WP-Nonce': wpApiSettings.nonce,
-	};
+	},
+	// ensures things work with cors.
+	credentials: 'same-origin',
+} );
 
+export const fetch = ( path: string, options: FetchOptions = {} ) => {
 	if ( options.body ) {
 		options.body = JSON.stringify( options.body );
 	}
 
-	// ensures things work with cors.
-	options.credentials = 'same-origin';
-
 	return {
 		type: 'FETCH',
 		path: `${ wpApiSettings.root }${ path }`,
-		options,
+		options: fetchCredOpts( options ),
 	};
 };
 
+export const fetchWithCreds = (
+	path: FetchArgs[ 'path' ],
+	options: FetchArgs[ 'options' ]
+) => {
+	return new Promise( ( resolve, reject ) => {
+		window
+			.fetch( path, fetchCredOpts( options ) )
+			.then( ( response ) => response.json() )
+			.then( ( result ) => resolve( result ) )
+			.catch( ( error ) => reject( error ) );
+	} );
+};
+
 export default {
-	FETCH( {
-		path,
-		options,
-	}: {
-		path: string;
-		options: { [ key: string ]: any };
-	} ) {
+	FETCH( { path, options }: FetchArgs ) {
 		return new Promise( ( resolve, reject ) => {
 			window
 				.fetch( path, options )
