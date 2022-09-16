d5727abc6e7e8c5cb40a474e1176065a3e2f87c7
Daniel Iser <daniel@code-atlantic.com>
2022-09-16 04:45:03 +0000

    Add TextHighlight component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as ControlledTabPanel } from './controlled-tab-panel';
 export { default as LinkControl } from './url-control';
 export { default as ListTable } from './list-table';
+export { default as TextHighlight } from './text-highlight';
 export { default as RadioButtonControl } from './radio-buttons-control';
 export { default as SearchableMulticheckControl } from './searchable-multicheck-control';
