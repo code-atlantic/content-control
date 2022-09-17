103e04fb341555d20c42c9fde0f4036239cb5cd0
Daniel Iser <daniel@code-atlantic.com>
2022-09-17 08:57:40 +0000

    Add upgrade icon

--- src/icons/lib/index.tsx
+++ src/icons/lib/index.tsx
@@ -3,3 +3,4 @@ export { default as incognito } from './incognito';
 export { default as lockedUser } from './locked-user';
 export { default as protectedMessage } from './protected-message';
 export { default as protectedRedirect } from './protected-redirect';
+export { default as upgrade } from './upgrade';
