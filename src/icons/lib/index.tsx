7f79b483dade8910aa53d01012847f9c238d708d
Daniel Iser <daniel@code-atlantic.com>
2022-09-19 09:46:04 +0000

    Add new icons

--- src/icons/lib/index.tsx
+++ src/icons/lib/index.tsx
@@ -1,6 +1,8 @@
+export { default as customRedirect } from './custom-redirect';
 export { default as controlledContent } from './controlled-content';
 export { default as incognito } from './incognito';
 export { default as lockedUser } from './locked-user';
+export { default as monitor } from './monitor';
 export { default as protectedMessage } from './protected-message';
 export { default as protectedRedirect } from './protected-redirect';
 export { default as upgrade } from './upgrade';
