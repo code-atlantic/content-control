b72ee64c9a3d1611efa1a5c77e2216dfa0f4a5b9
Daniel Iser <daniel@code-atlantic.com>
2022-09-21 04:20:52 +0000

    Add method to block editor that sends list of known block types

--- src/block-editor/block-extensions/index.tsx
+++ src/block-editor/block-extensions/index.tsx
@@ -1,1 +1,26 @@
 import './block-controls';
+
+import domReady from '@wordpress/dom-ready';
+import { getBlockTypes } from '@wordpress/blocks';
+
+import { fetchCredOpts, restUrl } from '@data';
+
+// When the block editor is loaded, send a list of registered block types to the server for use outside the editor.
+domReady( function () {
+	const blockTypes = getBlockTypes().map(
+		( { name, category, description, icon, keywords, title } ) => ( {
+			name,
+			category,
+			description,
+			keywords,
+			title,
+		} )
+	);
+
+	const options = {
+		method: 'POST',
+		body: JSON.stringify( blockTypes ),
+	};
+
+	fetch( `${ restUrl }blockTypes`, fetchCredOpts( options ) );
+} );
