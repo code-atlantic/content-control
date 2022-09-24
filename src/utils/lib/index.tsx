77b641b73f908ee241bc2629833eef0c6ef75dcd
Daniel Iser <daniel@code-atlantic.com>
2022-09-24 08:41:20 +0000

    Add useControlledState hook

--- src/utils/lib/index.tsx
+++ src/utils/lib/index.tsx
@@ -1,2 +1,3 @@
 export { default as cleanRestrictionData } from './clean-restriction-data';
 export { default as saveFile } from './save-file';
+export { default as useControlledState } from './use-controlled-state';
