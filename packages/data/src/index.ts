0f89bed6665906c5e7af20fa5dbb39ce553b8ba9
Daniel Iser <daniel@code-atlantic.com>
2022-10-11 08:51:51 +0000

    Convert to non ambient typings

--- packages/data/src/index.ts
+++ packages/data/src/index.ts
@@ -1,11 +1,27 @@
-import { createRegistry } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
+import { createRegistry } from '@wordpress/data';
 
 import {
-	settingsStore,
 	restrictionsStore,
+	settingsStore,
 	urlSearchStore,
 } from '@content-control/core-data';
+
+/* Broken @wordpress/data type overrides */
+declare module '@wordpress/data' {
+	function createRegistry(
+		storeConfigs?: Object,
+		parent?: Object | null
+	): {
+		registerGenericStore: Function;
+		registerStore: Function;
+		subscribe: Function;
+		select: Function;
+		dispatch: Function;
+		register: Function;
+		registerStoreInstance: Function;
+	};
+}
 
 const registry = createRegistry( {} );
 registry.register( coreStore );
