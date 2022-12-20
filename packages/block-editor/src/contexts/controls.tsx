da12599f3458227b9c64eeb74f832b0a06668b88
Daniel Iser <daniel@code-atlantic.com>
2022-12-20 05:26:46 +0000

    Add new context for block controls

---
+++ packages/block-editor/src/contexts/controls.tsx
@@ -0,0 +1,58 @@
+import { createContext, useContext } from '@wordpress/element';
+
+import type {
+	BlockControls,
+	ConditionalBlockControlsGroup,
+	DeviceBlockControlsGroup,
+} from '../block-extensions/block-controls/types';
+
+export const defaultBlockControls: BlockControls = {
+	enabled: false,
+};
+
+export const defaultDeviceBlockControls: DeviceBlockControlsGroup = {
+	hideOn: {
+		desktop: false,
+		tablet: false,
+		mobile: false,
+	},
+};
+
+export const defaultConditionBlockControls: ConditionalBlockControlsGroup = {
+	anyAll: 'any',
+	conditionSets: [],
+};
+
+type BlockControlsContextType = BlockControls & {
+	setBlockControls: ( blockControls: BlockControls ) => void;
+};
+
+export const BlockControlsContext: React.Context< BlockControlsContextType > =
+	createContext< BlockControlsContextType >( {} as BlockControlsContextType );
+
+export const BlockControlsContextProvider = ( {
+	children,
+	...context
+}: BlockControlsContextType & {
+	children: React.ReactNode;
+} ) => {
+	return (
+		<BlockControlsContext.Provider value={ context }>
+			{ children }
+		</BlockControlsContext.Provider>
+	);
+};
+
+const useBlockControls = () => {
+	const context = useContext( BlockControlsContext );
+
+	if ( context === undefined ) {
+		throw new Error(
+			'useBlockControls must be used within BlockControlsContext.Provider'
+		);
+	}
+
+	return context;
+};
+
+export default useBlockControls;
---
+++ packages/block-editor/src/contexts/index.tsx
@@ -0,0 +1,2 @@
+export * from './controls';
+export { default as useBlockControls } from './controls';
