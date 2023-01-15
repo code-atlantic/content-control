089923b1a52b4a8fe21f007655c036f46fa3cadb
Daniel Iser <daniel@code-atlantic.com>
2023-01-15 09:59:20 +0000

    Separate setRootList function from setList chaining

--- packages/rule-engine/src/components/query-list/index.tsx
+++ packages/rule-engine/src/components/query-list/index.tsx
@@ -8,6 +8,7 @@ import { __ } from '@wordpress/i18n';
 
 import { QueryContextProvider, useQuery } from '../../contexts';
 import Item from '../item';
+import { QueryListButtons } from './query-buttons';
 import Sortablelist from './sortable-list';
 
 import type {
@@ -16,7 +17,6 @@ import type {
 	QueryContextProps,
 	QueryProps,
 } from '../../types';
-import { QueryListButtons } from './query-buttons';
 
 type Props = QueryProps & {
 	indexs?: number[];
@@ -28,9 +28,10 @@ type Props = QueryProps & {
 const QueryList = ( { query, onChange, indexs = [] }: Props ) => {
 	const { items = [], logicalOperator } = query;
 
+	// Used for parent traversal when nested.
 	const parentQueryContext = useQuery();
 
-	const { setList: setRootList = false } = parentQueryContext;
+	const isRoot = typeof parentQueryContext.isRoot === 'undefined';
 
 	// Using controlled state allows the root list to control the isDragging state.
 	const [ isDragging, setIsDragging ] = useControlledState< boolean >(
@@ -39,25 +40,53 @@ const QueryList = ( { query, onChange, indexs = [] }: Props ) => {
 		parentQueryContext.setIsDragging
 	);
 
+	const setRootList: QueryContextProps[ 'setRootList' ] =
+		typeof parentQueryContext.setRootList !== 'undefined'
+			? parentQueryContext.setRootList
+			: ( newList ) => {
+					/**
+					 * The root list can accept a functional state action.
+					 * Check for that and call it with current items if needed.
+					 * This will be rootList in setList for non root query context.
+					 */
+					const _newList =
+						typeof newList !== 'function'
+							? newList
+							: // Root list passes in current state to updaters.
+							  newList( items );
+
+					// !! The list is managed by parent state, returning
+					// !! here breaks things such as items not being removed
+					// !! from lists on drag to another list.
+					// !! Leaving this here as a reminder.
+					//if ( isEqual( items, _newList ) ) {
+					// return; // !! Don't Do This !!
+					//}
+
+					// ** Don't save state changes while dragging.
+					if ( isDragging ) {
+						return;
+					}
+
+					console.log( 'setRootListSucceeded', _newList );
+
+					onChange( {
+						...query,
+						items: _newList,
+					} );
+			  };
 	/**
 	 * Generate a context to be provided to all children consumers of this query.
 	 */
 	const queryContext: QueryContextProps = {
 		...parentQueryContext,
-		isRoot: isRootList,
+		isRoot,
 		indexs,
 		isDragging,
 		setIsDragging,
 		logicalOperator,
 		query,
-		/**
-		 * The root setList method calls the onChange method directly.
-		 * Nested lists will then call setRootList and pass a SetStateFunctional
-		 * that modifies the rootList based on the current list indexs list.
-		 *
-		 * @param {ItemType[]} newList Array of current lists items.
-		 */
-		setList: isRootList
+		setRootList: isRoot
 			? ( newList ) => {
 					/**
 					 * The root list can accept a functional state action.
@@ -90,74 +119,79 @@ const QueryList = ( { query, onChange, indexs = [] }: Props ) => {
 						items: _newList,
 					} );
 			  }
-			: ( newList ) =>
-					setRootList( ( rootList ) => {
-						// Clone root list.
-						const rootListCopy = [ ...rootList ];
-						// Clone indexs to current list.
-						const parentIndexs = [ ...indexs ];
+			: parentQueryContext.setRootList,
 
-						// Remove this lists index from dex from cloned indexs.
-						const currentListIndex = parentIndexs.pop();
+		/**
+		 * The root setList method calls the onChange method directly.
+		 * Nested lists will then call setRootList and pass a SetStateFunctional
+		 * that modifies the rootList based on the current list indexs list.
+		 *
+		 * @param {ItemType[]} newList Array of current lists items.
+		 */
+		setList: ( newList ) =>
+			setRootList( ( rootList ) => {
+				// Clone root list.
+				const rootListCopy = [ ...rootList ];
+				// Clone indexs to current list.
+				const parentIndexs = [ ...indexs ];
 
-						// This is the root list.
-						if ( ! currentListIndex || isRootList ) {
-							return rootListCopy;
-						}
+				// Remove this lists index from cloned indexs.
+				const currentListIndex = parentIndexs.pop() ?? 0;
 
-						/**
-						 * Get reference to latest array in nested structure.
-						 *
-						 * This clever function loops over each parent indexs,
-						 * starting at the root, returning a reference to the
-						 * last & current nested list within the data structure.
-						 *
-						 * The accumulator start value is the entire root tree.
-						 *
-						 * Effectively drilling down from root -> current
-						 *
-						 * Return reference to child list for each parent index.
-						 */
-						const closestParentList = parentIndexs.reduce(
-							( arr, i ) => {
-								const nextParentGroup = arr[ i ] as GroupItem;
+				// This is the root list.
+				if ( isRoot ) {
+					return rootList;
+				}
 
-								if ( ! ( 'query' in nextParentGroup ) ) {
-									// Reference to child list for each parent index.
-									throw "Item's parent is not a group!";
-								}
-								return nextParentGroup.query.items;
-							},
-							rootListCopy
-						);
+				if ( isDragging ) {
+					return rootList;
+				}
 
-						const closestParentGroup =
-							closestParentList[ currentListIndex ];
+				/**
+				 * Get reference to latest array in nested structure.
+				 *
+				 * This clever function loops over each parent indexs,
+				 * starting at the root, returning a reference to the
+				 * last & current nested list within the data structure.
+				 *
+				 * The accumulator start value is the entire root tree.
+				 *
+				 * Effectively drilling down from root -> current
+				 *
+				 * Return reference to child list for each parent index.
+				 */
+				const closestParentList = parentIndexs.reduce( ( arr, i ) => {
+					const nextParentGroup = arr[ i ] as GroupItem;
 
-						if ( 'query' in closestParentGroup ) {
-							const _newList =
-								typeof newList === 'function'
-									? newList( rootList )
-									: newList;
-							// Prevent saving state if items are equal.
-							if (
-								isEqual(
-									closestParentGroup.query.items,
-									_newList
-								)
-							) {
-								return rootList;
-							}
+					if ( ! ( 'query' in nextParentGroup ) ) {
+						// Reference to child list for each parent index.
+						throw "Item's parent is not a group!";
+					}
+					return nextParentGroup.query.items;
+				}, rootListCopy );
 
-							// Replaced referenced items list with updated list.
-							closestParentGroup.query.items = _newList;
-						} else {
-							throw "Item's parent is not a group!";
-						}
+				// Get reference to current list from the parent list reference.
+				const closestParentGroup =
+					closestParentList[ currentListIndex ];
 
-						return rootListCopy;
-					} ),
+				if ( 'query' in closestParentGroup ) {
+					const _newList =
+						typeof newList === 'function'
+							? newList( rootList )
+							: newList;
+					// Prevent saving state if items are equal.
+					if ( isEqual( closestParentGroup.query.items, _newList ) ) {
+						return rootList;
+					}
 
+					// Replaced referenced items list with updated list.
+					closestParentGroup.query.items = _newList;
+				} else {
+					throw "Item's parent is not a group!";
+				}
+
+				return rootListCopy;
+			} ),
 		updateOperator: ( updatedOperator ) =>
 			onChange( {
 				...query,
@@ -199,7 +233,7 @@ const QueryList = ( { query, onChange, indexs = [] }: Props ) => {
 			<Sortablelist
 				className={ classNames( [
 					'cc-rule-engine-query-list',
-					isRootList ? 'is-root' : 'is-nested',
+					isRoot ? 'is-root' : 'is-nested',
 					items.length &&
 						( items.length > 1 ? 'has-items' : 'has-item' ),
 					isDragging && 'is-dragging',
