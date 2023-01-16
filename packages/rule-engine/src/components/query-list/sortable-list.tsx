3b227309e6952cf98d8c98e12ce55ad6dffa7479
Daniel Iser <daniel@code-atlantic.com>
2023-01-16 05:15:04 +0000

    Don't allow dropping groups in groups when nesting disabled

--- packages/rule-engine/src/components/query-list/sortable-list.tsx
+++ packages/rule-engine/src/components/query-list/sortable-list.tsx
@@ -1,7 +1,7 @@
 import classNames, { Argument as ClassNameArg } from 'classnames';
 import { ReactSortable } from 'react-sortablejs';
 
-import { useQuery } from '../../contexts';
+import { useOptions, useQuery } from '../../contexts';
 import { sortableConfig } from './sortable-options';
 
 import type { ReactNode } from 'react';
@@ -20,6 +20,9 @@ const SortableList = < I extends BaseItem >( {
 	className: ClassNameArg;
 	children?: ReactNode;
 } ): JSX.Element => {
+	const {
+		features: { nesting = false },
+	} = useOptions();
 	const { setIsDragging } = useQuery();
 
 	return (
@@ -35,6 +38,33 @@ const SortableList = < I extends BaseItem >( {
 			} }
 			onUnchoose={ () => {
 				setIsDragging( false );
+			} }
+			onMove={ ( event ) => {
+				// return false; — for cancel
+				// return -1; — insert before target
+				// return 1; — insert after target
+				// return true; — keep default insertion point based on the direction
+				// return void; — keep default insertion point based on the direction
+
+				// Prevent dragging group items into other groups if nesting is not enabled.
+				if ( nesting ) {
+					return true;
+				}
+
+				const { dragged, to } = event;
+
+				// if dragged element has cc-group-item class, prevent moving into nested lists.
+				if (
+					dragged.classList.contains(
+						'cc-rule-engine-item-wrapper--group'
+					) &&
+					to.classList.contains( 'is-nested' )
+				) {
+					console.log( 'Nesting groups is not currently allowed.' );
+					return false;
+				}
+
+				return true;
 			} }
 			{ ...sortableConfig }
 			{ ...additionalConfig }
