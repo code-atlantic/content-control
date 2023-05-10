49851d4b954013508715886ce283fea7156de129
Daniel Iser <daniel@code-atlantic.com>
2023-05-10 03:51:28 -0400

    Add filter for excerpts

--- classes/Controllers/Frontend.php
+++ classes/Controllers/Frontend.php
@@ -44,13 +44,17 @@ class Frontend extends Controller {
 	 * @return void
 	 */
 	public function hooks() {
-		add_filter( 'content_control/feed_restricted_message', '\ContentControl\append_post_excerpts', 10, 2 );
+		add_filter( 'content_control/feed_restricted_message', '\ContentControl\append_post_excerpts', 9, 2 );
 		add_filter( 'content_control/feed_restricted_message', 'wpautop', 10 );
 		add_filter( 'content_control/feed_restricted_message', 'do_shortcode', 10 );
 
-		add_filter( 'content_control/post_restricted_message', '\ContentControl\append_post_excerpts', 10, 2 );
-		add_filter( 'content_control/post_restricted_message', 'wpautop', 10 );
-		add_filter( 'content_control/post_restricted_message', 'do_shortcode', 10 );
+		add_filter( 'content_control/post_restricted_content', '\ContentControl\append_post_excerpts', 9, 2 );
+		add_filter( 'content_control/post_restricted_content', 'wpautop', 10 );
+		add_filter( 'content_control/post_restricted_content', 'do_shortcode', 10 );
+
+		add_filter( 'content_control/post_restricted_excerpt', '\ContentControl\append_post_excerpts', 9, 2 );
+		add_filter( 'content_control/post_restricted_excerpt', 'wpautop', 10 );
+		add_filter( 'content_control/post_restricted_excerpt', 'do_shortcode', 10 );
 	}
 
 }
--- classes/Controllers/Frontend/Posts.php
+++ classes/Controllers/Frontend/Posts.php
@@ -31,6 +31,7 @@ class Posts extends Controller {
 		}
 
 		add_action( 'the_content', [ $this, 'the_content' ], 1000 );
+		add_action( 'get_the_excerpt', [ $this, 'get_the_excerpt' ], 1000, 2 );
 	}
 
 	/**
@@ -43,6 +44,10 @@ class Posts extends Controller {
 	public function the_content( $content ) {
 		global $post;
 
+		if ( doing_filter( 'get_the_excerpt' ) ) {
+			return $content;
+		}
+
 		// If this isn't a post type that can be restricted, bail.
 		if ( ! $post || ! is_object( $post ) || $post->ID <= 0 ) {
 			return $content;
@@ -52,14 +57,46 @@ class Posts extends Controller {
 			return $content;
 		}
 
-		if ( ! content_is_restricted() ) {
+		if ( ! content_is_restricted( $post ) ) {
 			return $content;
 		}
 
 		$restriction = $this->container->get( 'restrictions' )->get_applicable_restriction();
 
 		return apply_filters(
-			'content_control/feed_restricted_message',
+			'content_control/post_restricted_content',
+			$restriction->get_message(),
+			$restriction
+		);
+	}
+
+	/**
+	 * Filter post excerpt when needed.
+	 *
+	 * @param string  $post_excerpt The post excerpt.
+	 * @param WP_Post $post         Post object.
+	 *
+	 * @return string
+	 */
+	public function get_the_excerpt( $post_excerpt, $post ) {
+
+		// If this isn't a post type that can be restricted, bail.
+		if ( ! $post || ! is_object( $post ) || $post->ID <= 0 ) {
+			return $post_excerpt;
+		}
+
+		if ( protection_is_disabled() ) {
+			return $post_excerpt;
+		}
+
+		if ( ! content_is_restricted( $post ) ) {
+			return $post_excerpt;
+		}
+
+		$restriction = $this->container->get( 'restrictions' )->get_applicable_restriction();
+
+		return apply_filters(
+			'content_control/post_restricted_excerpt',
 			$restriction->get_message(),
 			$restriction
 		);
