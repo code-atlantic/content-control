f58940e98aff9ad82f1ff80c5649a543a599197e
Daniel Iser <daniel@code-atlantic.com>
2023-05-18 04:19:59 -0400

    Update label for object select field

--- packages/fields/src/lib/object-select.tsx
+++ packages/fields/src/lib/object-select.tsx
@@ -108,7 +108,15 @@ const ObjectSelectField = ( {
 	return (
 		<div className="cc-object-search-field">
 			<SmartTokenControl
-				label={ label }
+				label={ sprintf(
+					__( '%s(s)', 'content-control' ),
+					entityType
+						.replace( /_/g, ' ' )
+						// uppercase first letter.
+						.charAt( 0 )
+						.toUpperCase() +
+						entityType.replace( /_/g, ' ' ).slice( 1 )
+				) }
 				multiple={ multiple }
 				placeholder={ sprintf(
 					__( 'Select %s(s)', 'content-control' ),
