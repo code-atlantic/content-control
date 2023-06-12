1447034383eeaf8f56b80c35e2e7b2ef63860939
Daniel Iser <daniel@code-atlantic.com>
2023-06-12 04:52:15 -0400

    Add finished v2 migration processes

--- classes/Migrations/Version2.php
+++ classes/Migrations/Version2.php
@@ -7,6 +7,8 @@
 
 namespace ContentControl\Migrations;
 
+defined( 'ABSPATH' ) || exit;
+
 use function ContentControl\plugin;
 
 /**
@@ -15,13 +17,32 @@ use function ContentControl\plugin;
 class Version2 {
 
 	/**
+	 * Stream.
+	 *
+	 * @var \ContentControl\Base\Stream
+	 */
+	private $stream;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->stream = new \ContentControl\Base\Stream( 'cc-v2-migration' );
+	}
+
+	/**
 	 * Run the migration.
 	 */
 	public function run() {
+		$this->stream->start();
+
+		$this->stream->send_event( 'start' );
+
 		$tasks = [
-			'migrate_user_meta',
-			'migrate_wp_options',
 			'migrate_restrictions',
+			'migrate_plugin_settings',
+			'migrate_user_meta',
+			'migrate_plugin_meta',
 		];
 
 		$t = 0;
@@ -32,23 +53,136 @@ class Version2 {
 		}
 
 		if ( count( $tasks ) === $t ) {
-			update_option( 'content_control_v2_ready', true );
+			$this->stream->send_event( 'complete' );
+
+			$data_versioning = \get_option( 'content_control_data_versioning', [] );
+
+			$data_versioning['settings']     = 2;
+			$data_versioning['restrictions'] = 2;
+
+			\update_option( 'content_control_data_versioning', $data_versioning );
 		}
 	}
 
 	/**
-	 * Convert object to array.
+	 * Migrate restrictions from options to post type.
 	 *
-	 * @param object $obj Object to convert.
-	 * @return array
+	 * @param array $restrictions Array of restrictions.
+	 *
+	 * @return void
 	 */
-	public function object_to_array( $obj ) {
-		$array = [];
-		foreach ( (array) $obj as $key => $value ) {
-			$array[ $key ] = is_object( $value ) || is_array( $value ) ? self::object_to_array( $value ) : $value;
+	public function migrate_restrictions( $restrictions = [] ) {
+		$settings = \get_option( 'jp_cc_settings', [] );
+
+		$count = isset( $settings['restrictions'] ) ? count( $settings['restrictions'] ) : 0;
+
+		if ( $count ) {
+			$this->stream->send_event(
+				'task:start',
+				[
+					'task'  => 'migrate_restrictions',
+					'total' => $count,
+				]
+			);
+
+			foreach ( $restrictions as $key => $restriction ) {
+				if ( $this->migrate_restriction( $restriction ) ) {
+					// Unset any restrictions that were migrated.
+					unset( $settings['restrictions'][ $key ] );
+
+					$this->stream->send_error(
+						'task:progress',
+						[
+							'task'     => 'migrate_restrictions',
+							'progress' => $key + 1,
+						]
+					);
+				} else {
+					$this->stream->send_error(
+						'task:error',
+						[
+							'task'        => 'migrate_restrictions',
+							'progress'    => $key + 1,
+							'message'     => 'Failed to migrate restriction.',
+							'restriction' => $restriction,
+						]
+					);
+				}
+			}
 		}
 
-		return $array;
+		if ( empty( $settings['restrictions'] ) ) {
+			unset( $settings['restrictions'] );
+
+			$this->stream->send_event(
+				'task:complete',
+				[
+					'task' => 'migrate_restrictions',
+				]
+			);
+		} else {
+			$this->stream->send_error(
+				'task:error',
+				[
+					'task'         => 'migrate_restrictions',
+					'message'      => 'Failed to migrate all restrictions.',
+					'restrictions' => $settings['restrictions'],
+				]
+			);
+		}
+
+		// Update the settings with any migrated restrictions removed.
+		\update_option( 'jp_cc_settings', $settings );
+	}
+
+	/**
+	 * Migrate a given restriction to the new post type.
+	 *
+	 * @param array $restriction Restriction data.
+	 *
+	 * @return bool True if successful, false otherwise.
+	 */
+	public function migrate_restriction( $restriction ) {
+		$restriction = \wp_parse_args( $restriction, [
+			'title'                    => '',
+			'who'                      => '',
+			'roles'                    => [],
+			'protection_method'        => 'redirect',
+			'show_excerpts'            => false,
+			'override_default_message' => false,
+			'custom_message'           => '',
+			'redirect_type'            => 'login',
+			'redirect_url'             => '',
+			'conditions'               => '',
+		]);
+
+		$new_restriction_id = \wp_insert_post(
+			[
+				'post_type'    => 'cc_restriction',
+				'post_title'   => $restriction['title'],
+				'post_content' => $restriction['custom_message'],
+				'post_status'  => 'publish',
+			]
+		);
+
+		$user_roles = is_array( $restriction['roles'] ) ? $restriction['roles'] : [];
+
+		$settings = [
+			'userStatus'       => $restriction['who'],
+			'roleMatch'        => count( $user_roles ) > 0 ? 'match' : 'any',
+			'userRoles'        => $user_roles,
+			'protectionMethod' => 'custom_message' === $restriction['protection_method'] ? 'message' : 'redirect',
+			'redirectType'     => $restriction['redirect_type'],
+			'redirectUrl'      => \sanitize_url( $restriction['redirect_url'] ),
+			'overrideMessage'  => $restriction['override_default_message'],
+			'customMessage'    => $restriction['custom_message'],
+			'showExcerpts'     => $restriction['show_excerpts'],
+			'conditions'       => \ContentControl\remap_conditions_to_query( $restriction['conditions'] ),
+		];
+
+		$added_meta = \add_post_meta( $new_restriction_id, 'restriction_settings', $settings, true );
+
+		return $new_restriction_id > 0 && $added_meta;
 	}
 
 	/**
@@ -56,15 +190,27 @@ class Version2 {
 	 *
 	 * @return void
 	 */
-	public function migrate_settings() {
-		$settings = get_option( 'jp_cc_settings', [] );
+	public function migrate_plugin_settings() {
+		$this->stream->send_event(
+			'task:start',
+			[
+				'task' => 'migrate_plugin_settings',
+			]
+		);
 
-		if ( ! empty( $settings['restrictions'] ) ) {
-			$this->migrate_restrictions( $settings['restrictions'] );
+		$settings             = get_option( 'jp_cc_settings', [] );
+		$defaultDenialMessage = isset( $settings['default_denial_message'] ) ? $settings['default_denial_message'] : '';
+
+		if ( ! empty( $defaultDenialMessage ) ) {
+			plugin()->get( 'options' )->update( 'defaultDenialMessage', $defaultDenialMessage );
 		}
 
-		// TODO
-		// - Remap default_denial_message to defaultDenialMessage.
+		$this->stream->send_event(
+			'task:complete',
+			[
+				'task' => 'migrate_plugin_settings',
+			]
+		);
 	}
 
 	/**
@@ -73,10 +219,37 @@ class Version2 {
 	 * @return void
 	 */
 	public function migrate_user_meta() {
+		global $wpdb;
+
+		$this->stream->send_event(
+			'task:start',
+			[
+				'task' => 'migrate_user_meta',
+			]
+		);
+
 		$remapped_keys = [
-			'_jp_cc_reviews_dismissed_triggers' => '_content_control_reviews_dismissed_triggers',
-			'_jp_cc_reviews_last_dismissed'     => '_content_control_reviews_last_dismissed',
+			'_jp_cc_reviews_dismissed_triggers' => 'content_control_reviews_dismissed_triggers',
+			'_jp_cc_reviews_last_dismissed'     => 'content_control_reviews_last_dismissed',
 		];
+
+		// Update all keys via $wpdb.
+		foreach ( $remapped_keys as $old_key => $new_key ) {
+			$wpdb->query(
+				$wpdb->prepare(
+					"UPDATE {$wpdb->usermeta} SET meta_key = %s WHERE meta_key = %s",
+					$new_key,
+					$old_key
+				)
+			);
+		}
+
+		$this->stream->send_event(
+			'task:complete',
+			[
+				'task' => 'migrate_user_meta',
+			]
+		);
 	}
 
 	/**
@@ -84,7 +257,14 @@ class Version2 {
 	 *
 	 * @return void
 	 */
-	public function migrate_wp_options() {
+	public function migrate_plugin_meta() {
+		$this->stream->send_event(
+			'task:start',
+			[
+				'task' => 'migrate_plugin_meta',
+			]
+		);
+
 		$remaps = [
 			'jp_cc_reviews_installed_on' => 'content_control_installed_on',
 		];
@@ -97,94 +277,13 @@ class Version2 {
 				delete_option( $key );
 			}
 		}
-	}
 
-	/**
-	 * Migrate widgets.
-	 *
-	 * @return void
-	 */
-	public function migrate_widgets() {
-		// Do the option keys stay the same? If so probably need to do nothing.
-	}
-
-	/**
-	 * Migrate restrictions from options to post type.
-	 *
-	 * @param array $restrictions Array of restrictions.
-	 *
-	 * @return void
-	 */
-	public function migrate_restrictions( $restrictions = [] ) {
-		$restrictions = $this->object_to_array( $restrictions );
-
-		// Remove array keys.
-		$restrictions = array_values( $restrictions );
-
-		$old = [
-			'title'                    => '',
-			'who'                      => '',
-			'roles'                    => [],
-			'conditions'               => '',
-			'redirect_url'             => '',
-			'redirect_type'            => 'login',
-			'custom_message'           => '',
-			'override_default_message' => false,
-			'show_excerpts'            => false,
-			'protection_method'        => 'redirect',
-		];
-
-		$remap = [
-			'who'       => 'userStatus',
-			'roles'     => 'userRoles',
-			'roleMatch' => 'match', // TODO 'any' if roles is empty.
-		];
-
-		// TODO - Replace post_content with custom_message, or default global message.
-	}
-
-	/**
-	 * Remap old conditions to new rules.
-	 *
-	 * @param array $conditions Array of old conditions.
-	 * @return array
-	 */
-	public function remap_conditions_to_rules( $conditions ) {
-		$old_rules = [
-			'{$post_type}_index'         => [],
-			'{$post_type}_all'           => [],
-			'{$post_type}_selected'      => [
-				'selected' => [],
-			],
-			'{$post_type}_ID'            => [
-				'selected' => '',
-			],
-			'{$post_type}_children'      => [
-				'selected' => [],
-			],
-			'{$post_type}_ancestors'     => [
-				'selected' => '',
-			],
-			'{$post_type}_template'      => [
-				'selected' => [],
-			],
-			'{$post_type}_w_{$tax_name}' => [
-				'selected' => [],
-			],
-			'tax_{$tax_name}_all'        => [],
-			'tax_{$tax_name}_selected'   => [
-				'selected' => [],
-			],
-			'tax_{$tax_name}_ID'         => [
-				'selected' => '',
-			],
-			'is_front_page'              => [],
-			'is_home'                    => [],
-			'is_search'                  => [],
-			'is_404'                     => [],
-		];
-
-		return $conditions;
+		$this->stream->send_event(
+			'task:complete',
+			[
+				'task' => 'migrate_plugin_meta',
+			]
+		);
 	}
 
 }
