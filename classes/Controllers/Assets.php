bb7f979c1877961b8ad9af8f4ace3772ae6939c9
Daniel Iser <daniel@code-atlantic.com>
2023-06-13 04:22:50 -0400

    Fix incorrect variable usage

--- classes/Controllers/Assets.php
+++ classes/Controllers/Assets.php
@@ -140,7 +140,7 @@ class Assets extends Controller {
 
 			if ( isset( $package_data['varsName'] ) && ! empty( $package_data['vars'] ) ) {
 				$localized_vars = apply_filters( "content_control/{$package}_localized_vars", $package_data['vars'] );
-				wp_localize_script( $handle, $package_data['varsName'], $package_data['vars'] );
+				wp_localize_script( $handle, $package_data['varsName'], $localized_vars );
 			}
 
 			/**
