8c3921b44247b900c09f2abc57314a47c7dd5874
Daniel Iser <daniel@code-atlantic.com>
2023-07-28 17:31:45 -0400

    No need for function exists check

--- inc/functions/compatibility.php
+++ inc/functions/compatibility.php
@@ -22,41 +22,39 @@ function is_func_disabled( $func ) {
 	return in_array( $func, $disabled, true );
 }
 
-if ( ! function_exists( 'is_rest' ) ) {
-	/**
-	 * Checks if the current request is a WP REST API request.
-	 *
-	 * Case #1: After WP_REST_Request initialisation
-	 * Case #2: Support "plain" permalink settings and check if `rest_route` starts with `/`
-	 * Case #3: It can happen that WP_Rewrite is not yet initialized,
-	 *          so do this (wp-settings.php)
-	 * Case #4: URL Path begins with wp-json/ (your REST prefix)
-	 *          Also supports WP installations in subfolders
-	 *
-	 * @returns boolean
-	 * @author matzeeable
-	 */
-	function is_rest() {
-		// phpcs:disable WordPress.Security.NonceVerification.Recommended
-		if ( defined( 'REST_REQUEST' ) && REST_REQUEST // (#1)
-				|| isset( $_GET['rest_route'] ) // (#2)
-						&& strpos( sanitize_text_field( wp_unslash( $_GET['rest_route'] ) ), '/', 0 ) === 0 ) {
-				return true;
-		}
-
-		// (#3)
-		global $wp_rewrite;
-		if ( null === $wp_rewrite ) {
-			// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-			$wp_rewrite = new \WP_Rewrite();
-		}
-
-		// (#4)
-		$rest_url    = wp_parse_url( trailingslashit( rest_url() ) );
-		$current_url = wp_parse_url( add_query_arg( [] ) );
-		return strpos( $current_url['path'] ? $current_url['path'] : '/', $rest_url['path'], 0 ) === 0;
-		// phpcs:enable WordPress.Security.NonceVerification.Recommended
+/**
+ * Checks if the current request is a WP REST API request.
+ *
+ * Case #1: After WP_REST_Request initialisation
+ * Case #2: Support "plain" permalink settings and check if `rest_route` starts with `/`
+ * Case #3: It can happen that WP_Rewrite is not yet initialized,
+ *          so do this (wp-settings.php)
+ * Case #4: URL Path begins with wp-json/ (your REST prefix)
+ *          Also supports WP installations in subfolders
+ *
+ * @returns boolean
+ * @author matzeeable
+ */
+function is_rest() {
+	// phpcs:disable WordPress.Security.NonceVerification.Recommended
+	if ( defined( 'REST_REQUEST' ) && REST_REQUEST // (#1)
+			|| isset( $_GET['rest_route'] ) // (#2)
+					&& strpos( sanitize_text_field( wp_unslash( $_GET['rest_route'] ) ), '/', 0 ) === 0 ) {
+			return true;
 	}
+
+	// (#3)
+	global $wp_rewrite;
+	if ( null === $wp_rewrite ) {
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$wp_rewrite = new \WP_Rewrite();
+	}
+
+	// (#4)
+	$rest_url    = wp_parse_url( trailingslashit( rest_url() ) );
+	$current_url = wp_parse_url( add_query_arg( [] ) );
+	return strpos( $current_url['path'] ? $current_url['path'] : '/', $rest_url['path'], 0 ) === 0;
+	// phpcs:enable WordPress.Security.NonceVerification.Recommended
 }
 
 /**
