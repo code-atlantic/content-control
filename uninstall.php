66ea002252dea01ec1d1482ecd454a981a6be6a5
Daniel Iser <daniel@code-atlantic.com>
2023-08-18 04:15:22 -0400

    Add comments for data to delete on uninstall

--- uninstall.php
+++ uninstall.php
@@ -4,3 +4,14 @@
  *
  * @package ContentControl
  */
+
+
+//  'content_control_license';
+//  'content_control_pro_activation_date';
+// 'content_control_installed_on';
+// 'content_control_connect_token';
+// 'content_control_version';
+// 'content_control_data_versioning';
+// 'content_control_debug_log_token'; // delete log first.
+// 'content_control_known_blockTypes';
+// 'content_control_completed_upgrades';
