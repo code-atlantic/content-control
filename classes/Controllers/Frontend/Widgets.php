f49a3e7f3d1af4ea60ae4fe39bb1a86f60ba92a0
Daniel Iser <daniel@code-atlantic.com>
2023-08-20 04:00:27 -0400

    Ensure widgets respect protection_is_disabled

--- classes/Controllers/Frontend/Widgets.php
+++ classes/Controllers/Frontend/Widgets.php
@@ -14,7 +14,7 @@ use ContentControl\Base\Controller;
 
 use WP_Customize_Manager;
 
-use function ContentControl\user_is_excluded;
+use function ContentControl\protection_is_disabled;
 use function ContentControl\user_meets_requirements;
 use function ContentControl\Widgets\get_options as get_widget_options;
 
@@ -38,7 +38,7 @@ class Widgets extends Controller {
 	 * @return array The modified $widget_area array.
 	 */
 	public function exclude_widgets( $widget_areas ) {
-		if ( user_is_excluded() || is_admin() || $this->is_customize_preview() ) {
+		if ( protection_is_disabled() || $this->is_customize_preview() ) {
 			return $widget_areas;
 		}
 
