634804b0abcb3b3fc27c02b240a6029a222df22d
Daniel Iser <daniel@code-atlantic.com>
2023-09-06 21:13:26 -0400

    Fix lint issues in widget editor package

--- packages/widget-editor/src/index.ts
+++ packages/widget-editor/src/index.ts
@@ -2,32 +2,32 @@ import $ from 'jquery';
 
 import './index.scss';
 
-function which_users( this: HTMLElement ) {
-	var $this = $( this ),
+function whichUsers( this: HTMLElement ) {
+	const $this = $( this ),
 		$item = $this.parents( '.widget' ).eq( 0 ),
 		$roles = $item.find( '.widget_options-roles' );
 
-	if ( $this.val() == 'logged_in' ) {
+	if ( $this.val() === 'logged_in' ) {
 		$roles.show();
 	} else {
 		$roles.hide();
 	}
 }
 
-function refresh_all_items() {
+function refreshAllItems() {
 	$( '.widget_options-which_users select' ).each( function () {
-		which_users.call( this );
+		whichUsers.call( this );
 	} );
 }
 
 $( () => {
 	'use strict';
 
-	refresh_all_items();
+	refreshAllItems();
 
 	$( document )
 		.on( 'change', '.widget_options-which_users select', function () {
-			which_users.call( this );
+			whichUsers.call( this );
 		} )
-		.on( 'widget-updated', refresh_all_items );
+		.on( 'widget-updated', refreshAllItems );
 } );
