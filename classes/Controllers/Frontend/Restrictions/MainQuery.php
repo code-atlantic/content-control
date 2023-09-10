5f2d47479a05b1ebd927f38e9fe89200378155f8
Daniel Iser <daniel@code-atlantic.com>
2023-09-10 03:48:50 -0400

    Refactor to split responsibility and enforce context

--- classes/Controllers/Frontend/Restrictions/MainQuery.php
+++ classes/Controllers/Frontend/Restrictions/MainQuery.php
@@ -11,15 +11,14 @@ namespace ContentControl\Controllers\Frontend\Restrictions;
 use ContentControl\Base\Controller;
 
 use function ContentControl\redirect;
+use function ContentControl\get_main_wp_query;
 use function ContentControl\set_query_to_page;
-use function ContentControl\content_is_restricted;
+use function ContentControl\reset_query_context;
+use function ContentControl\query_can_be_ignored;
+use function ContentControl\override_query_context;
 use function ContentControl\protection_is_disabled;
 use function ContentControl\get_applicable_restriction;
-use function ContentControl\get_main_wp_query;
 use function ContentControl\get_restriction_matches_for_queried_posts;
-use function ContentControl\override_query_context;
-use function ContentControl\query_can_be_ignored;
-use function ContentControl\reset_query_context;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -37,77 +36,86 @@ class MainQuery extends Controller {
 		// This can be done no later than template_redirect, and no sooner than send_headers (when conditional tags are available).
 		// Can be done on send_headers, posts_selection, or wp as well.
 		add_action( 'template_redirect', [ $this, 'restrict_main_query' ], 10 );
-		add_action( 'template_redirect', [ $this, 'restrict_main_query_posts' ], 10 );
 	}
 
 	/**
 	 * Handle a restriction on the main query.
+	 *
+	 * NOTE: This is only for redirecting or replacing pages and
+	 *       should not be used to filter or hide post contents.
 	 */
 	public function restrict_main_query() {
 		if ( ! \is_main_query() || protection_is_disabled() ) {
 			return;
 		}
 
-		if ( ! content_is_restricted() ) {
-			return;
-		}
+		$this->check_main_query();
+		$this->check_main_query_posts();
+	}
 
+	/**
+	 * Handle restrictions on the main query.
+	 *
+	 * NOTE: This is only for redirecting or replacing archives and
+	 *       should not be used to filter or hide post contents.
+	 */
+	public function check_main_query() {
 		$restriction = get_applicable_restriction();
 
 		// Bail if we didn't match any restrictions.
-		if ( ! $restriction ) {
-			return;
-		}
-
-		/**
-		 * Use this filter to prevent a post from being restricted, or to handle it yourself.
-		 *
-		 * @param null                               $pre        Whether to prevent the post from being restricted.
-		 * @param null|\ContentControl\Models\Restriction $restriction Restriction object.
-		 * @return null|mixed
-		 */
-		if ( null !== apply_filters( 'content_control/pre_restrict_main_query', null, $restriction ) ) {
-			return;
-		}
-
-		/**
-		 * Fires when a post is restricted, but before the restriction is handled.
-		 *
-		 * @param \ContentControl\Models\Restriction $restriction Restriction object.
-		 */
-		do_action( 'content_control/restrict_main_query', $restriction );
-
-		switch ( $restriction->protection_method ) {
-			case 'redirect':
-				redirect( $restriction->redirect_type, $restriction->redirect_url );
+		if ( false !== $restriction ) {
+			/**
+			 * Use this filter to prevent a post from being restricted, or to handle it yourself.
+			 *
+			 * @param null                               $pre        Whether to prevent the post from being restricted.
+			 * @param null|\ContentControl\Models\Restriction $restriction Restriction object.
+			 * @return null|mixed
+			 */
+			if ( null !== apply_filters( 'content_control/pre_restrict_main_query', null, $restriction ) ) {
 				return;
+			}
 
-			case 'replace':
-				if ( 'page' === $restriction->replacement_type ) {
-					set_query_to_page( $restriction->replacement_page );
+			/**
+			 * Fires when a post is restricted, but before the restriction is handled.
+			 *
+			 * @param \ContentControl\Models\Restriction $restriction Restriction object.
+			 */
+			do_action( 'content_control/restrict_main_query', $restriction );
+
+			switch ( $restriction->protection_method ) {
+				case 'redirect':
+					redirect( $restriction->redirect_type, $restriction->redirect_url );
 					return;
-				}
+
+				case 'replace':
+					if ( 'page' === $restriction->replacement_type ) {
+						set_query_to_page( $restriction->replacement_page );
+						return;
+					}
+			}
 		}
 	}
 
 	/**
-	 * Handle a restriction on the main query posts.
+	 * Handle restrictions on the main query posts.
+	 *
+	 * NOTE: This is only for redirecting or replacing archives and
+	 *       should not be used to filter or hide post contents.
 	 */
-	public function restrict_main_query_posts() {
-		if ( protection_is_disabled() ) {
-			return;
-		}
-
+	public function check_main_query_posts() {
 		$query = get_main_wp_query();
 
 		if ( query_can_be_ignored( $query ) ) {
 			return;
 		}
 
+		// Ensure rules are checked in the correct context.
 		override_query_context( 'main/posts' );
 
+		// Get restrictions for the queried posts.
 		$post_restrictions = get_restriction_matches_for_queried_posts( $query );
 
+		// Reset query context.
 		reset_query_context();
 
 		if ( ! $post_restrictions ) {
