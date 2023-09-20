85d738b4246a59a50f5fc6767cd368cc189d2be2
Daniel Iser <daniel@code-atlantic.com>
2023-09-20 16:09:35 -0400

    Update phpstan types

--- bin/phpstan-bootstrap.php
+++ bin/phpstan-bootstrap.php
@@ -6,21 +6,49 @@
  */
 
 if ( ! defined( 'CONTENT_CONTROL_LICENSE_KEY' ) ) {
-	define( 'CONTENT_CONTROL_LICENSE_KEY', 'some+key' );
+	/**
+	 * License key.
+	 *
+	 * @phpstan-type string $license_key
+	 * @var string $license_key
+	 */
+	define( 'CONTENT_CONTROL_LICENSE_KEY', '' );
 }
 
 if ( ! defined( 'CONTENT_CONTROL_DISABLE_LOGGING' ) ) {
-	define( 'CONTENT_CONTROL_DISABLE_LOGGING', true );
+	/**
+	 * Disable logging.
+	 *
+	 * @phpstan-type bool $disable_logging
+	 * @var bool $disable_logging
+	 */
+	define( 'CONTENT_CONTROL_DISABLE_LOGGING', false );
 }
 
 if ( ! defined( 'IS_WPCOM' ) ) {
-	define( 'IS_WPCOM', true );
+	/**
+	 * Is WPCOM.
+	 *
+	 * @phpstan-type bool $is_wpcom
+	 * @var bool $is_wpcom
+	 */
+	define( 'IS_WPCOM', false );
 }
 
 if ( ! defined( 'WP_PLUGIN_DIR' ) ) {
+	/**
+	 * Plugin directory.
+	 *
+	 * @var string $wp_plugin_dir
+	 */
 	define( 'WP_PLUGIN_DIR', '/path/to/wp-content/plugins' );
 }
 
 if ( ! defined( 'WP_CONTENT_DIR' ) ) {
+	/**
+	 * Content directory.
+	 *
+	 * @var string $wp_content_dir
+	 */
 	define( 'WP_CONTENT_DIR', '/path/to/wp-content' );
 }
