ab296bca8381e27d04bc8ffca06652bc4629387f
Daniel Iser <daniel@code-atlantic.com>
2023-12-08 00:11:15 -0500

    Convert to compnent fields

--- packages/settings-page/src/restrictions-view/edit/general.tsx
+++ packages/settings-page/src/restrictions-view/edit/general.tsx
@@ -1,5 +1,6 @@
 import {
 	FieldPanel,
+	FieldRow,
 	RadioButtonControl,
 	SearchableMulticheckControl,
 } from '@content-control/components';
@@ -13,6 +14,7 @@ import {
 import { __ } from '@wordpress/i18n';
 import { search } from '@wordpress/icons';
 import { addFilter } from '@wordpress/hooks';
+import { useInstanceId } from '@wordpress/compose';
 import { type Restriction } from '@content-control/core-data';
 
 import useFields from '../use-fields';
@@ -22,6 +24,129 @@ import type { EditTabProps } from '.';
 
 /* Global Var Imports */
 const { userRoles } = contentControlSettingsPage;
+
+const GeneralWhoFields = ( {
+	settings,
+	updateSettings,
+}: {
+	settings: Restriction[ 'settings' ];
+	updateSettings: ( settings: Partial< Restriction[ 'settings' ] > ) => void;
+} ) => {
+	const instanceId = useInstanceId( GeneralWhoFields );
+
+	// ** TODO REVIEW -  This is here to ensure old data does not throw errors.
+	// ** It may be that if we have dedicated migration routine this can be removed.
+	let cleanedRoles: string[] = [];
+
+	if ( Array.isArray( settings.userRoles ) ) {
+		cleanedRoles = settings.userRoles;
+	} else if ( typeof settings.userRoles === 'object' ) {
+		cleanedRoles = Object.entries( settings.userRoles ).map(
+			( [ value ] ) => value
+		);
+	}
+
+	return (
+		<FieldPanel title={ __( 'User Status', 'content-control' ) }>
+			<RadioButtonControl
+				label={ __( 'Who can see this content?', 'content-control' ) }
+				value={ settings.userStatus }
+				onChange={ ( newUserStatus ) =>
+					updateSettings( {
+						userStatus: newUserStatus,
+					} )
+				}
+				options={ userStatusOptions() }
+				className="userStatus-field"
+			/>
+			{ 'logged_in' === settings.userStatus && (
+				<>
+					<br />
+					<hr />
+					<FieldRow
+						id={ `content-control-role-match-${ instanceId }` }
+						label={ __( 'User Role', 'content-control' ) }
+						description={ __(
+							'Which user roles should be allowed to see this content.',
+							'content-control'
+						) }
+						className="components-base-control__label"
+					>
+						<SelectControl
+							id={ `content-control-role-match-${ instanceId }` }
+							label={ __( 'User Role', 'content-control' ) }
+							value={ settings.roleMatch ?? 'any' }
+							options={ [
+								{
+									label: __( 'Any', 'content-control' ),
+									value: 'any',
+								},
+								{
+									label: __( 'Matching', 'content-control' ),
+									value: 'match',
+								},
+								{
+									label: __( 'Excluding', 'content-control' ),
+									value: 'exclude',
+								},
+							] }
+							onChange={ ( newRoleMatch ) =>
+								updateSettings( {
+									roleMatch: newRoleMatch as
+										| 'any'
+										| 'match'
+										| 'exclude',
+								} )
+							}
+							className="is-large roleMatch-field"
+							hideLabelFromVision={ true }
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+						/>
+					</FieldRow>
+
+					{ 'any' !== settings.roleMatch && (
+						<>
+							<hr />
+							<br />
+							<SearchableMulticheckControl
+								label={
+									'exclude' === settings.roleMatch
+										? __(
+												'Roles to exclude',
+												'content-control'
+										  )
+										: __(
+												'Roles to include',
+												'content-control'
+										  )
+								}
+								searchIcon={ search }
+								placeholder={ __(
+									'Search roles…',
+									'content-control'
+								) }
+								className="is-large userRoles-field"
+								value={ cleanedRoles }
+								onChange={ ( roles ) =>
+									updateSettings( {
+										userRoles: roles,
+									} )
+								}
+								options={ Object.entries( userRoles ).map(
+									( [ value, label ] ) => ( {
+										value,
+										label,
+									} )
+								) }
+							/>
+						</>
+					) }
+				</>
+			) }
+		</FieldPanel>
+	);
+};
 
 addFilter(
 	'contentControl.restrictionEditor.tabFields',
@@ -36,121 +161,18 @@ addFilter(
 			settings: Partial< Restriction[ 'settings' ] >
 		) => void
 	) => {
-		// ** TODO REVIEW -  This is here to ensure old data does not throw errors.
-		// ** It may be that if we have dedicated migration routine this can be removed.
-		let cleanedRoles: string[] = [];
+		const componentProps = {
+			settings,
+			updateSettings,
+		};
 
-		if ( Array.isArray( settings.userRoles ) ) {
-			cleanedRoles = settings.userRoles;
-		} else if ( typeof settings.userRoles === 'object' ) {
-			cleanedRoles = Object.entries( settings.userRoles ).map(
-				( [ value ] ) => value
-			);
-		}
 		return {
 			...fields,
 			general: [
 				{
 					id: 'userStatus',
 					priority: 3,
-					component: (
-						<FieldPanel
-							title={ __( 'User Status', 'content-control' ) }
-						>
-							<RadioButtonControl
-								label={ __(
-									'Who can see this content?',
-									'content-control'
-								) }
-								value={ settings.userStatus }
-								onChange={ ( newUserStatus ) =>
-									updateSettings( {
-										userStatus: newUserStatus,
-									} )
-								}
-								options={ userStatusOptions() }
-								className="userStatus-field"
-							/>
-
-							{ 'logged_in' === settings.userStatus && (
-								<>
-									<SelectControl
-										label={ __(
-											'User Role',
-											'content-control'
-										) }
-										value={ settings.roleMatch ?? 'any' }
-										options={ [
-											{
-												label: __(
-													'Any',
-													'content-control'
-												),
-												value: 'any',
-											},
-											{
-												label: __(
-													'Matching',
-													'content-control'
-												),
-												value: 'match',
-											},
-											{
-												label: __(
-													'Excluding',
-													'content-control'
-												),
-												value: 'exclude',
-											},
-										] }
-										onChange={ ( newRoleMatch ) =>
-											updateSettings( {
-												roleMatch: newRoleMatch as
-													| 'any'
-													| 'match'
-													| 'exclude',
-											} )
-										}
-										className="is-large roleMatch-field"
-									/>
-
-									{ 'any' !== settings.roleMatch && (
-										<SearchableMulticheckControl
-											label={
-												'exclude' === settings.roleMatch
-													? __(
-															'Roles to exclude',
-															'content-control'
-													  )
-													: __(
-															'Roles to include',
-															'content-control'
-													  )
-											}
-											searchIcon={ search }
-											placeholder={ __(
-												'Search roles…',
-												'content-control'
-											) }
-											className="is-large userRoles-field"
-											value={ cleanedRoles }
-											onChange={ ( roles ) =>
-												updateSettings( {
-													userRoles: roles,
-												} )
-											}
-											options={ Object.entries(
-												userRoles
-											).map( ( [ value, label ] ) => ( {
-												value,
-												label,
-											} ) ) }
-										/>
-									) }
-								</>
-							) }
-						</FieldPanel>
-					),
+					component: <GeneralWhoFields { ...componentProps } />,
 				},
 			],
 		};
