409fd26e5e6407a8734c34c90ba22c574286a4a3
Daniel L. Iser <daniel@code-atlantic.com>
2026-09-20 22:02:45 -0400

    Fix: Use plugin text domain for requirement notices

--- classes/Plugin/Prerequisites.php
+++ classes/Plugin/Prerequisites.php
@@ -311,8 +311,7 @@ class Prerequisites {
 		$message = __( 'This plugin requires <b>%1$s %2$s</b> or higher in order to run.', 'content-control' );
 		return sprintf(
 			$message,
-			// phpcs:ignore WordPress.WP.I18n.TextDomainMismatch -- Reuse the WordPress Core translation.
-			__( 'PHP', 'default' ),
+			__( 'PHP', 'content-control' ),
 		$failed_check_args['version'] );
 	}
 
@@ -328,8 +327,7 @@ class Prerequisites {
 		$message = __( 'This plugin requires <b>%1$s %2$s</b> or higher in order to run.', 'content-control' );
 		return sprintf(
 			$message,
-			// phpcs:ignore WordPress.WP.I18n.TextDomainMismatch -- Reuse the WordPress Core translation.
-			__( 'WordPress', 'default' ),
+			__( 'WordPress', 'content-control' ),
 			$failed_check_args['version']
 		);
 	}
